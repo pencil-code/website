ff25402e404dc5c45f8409be8d5308f3160098ec
Axel Brandenburg <brandenb@nordita.org>
2014-12-25 07:27:47 +0000

    current

--- news.php
+++ news.php
@@ -6,8 +6,8 @@
 <h2>News</h2>
 <ul>
 
-<li class="separated">25 Jan 2014:<br>
-The <a href=http://agenda.albanova.se/conferenceDisplay.py?confId=4213>10th Pencil Code Meeting</a> will take place 7-11 July 2014 in G&ouml;ttingen.
+<li class="separated">24 Dec 2014:<br>
+The <a href=http://agenda.albanova.se/conferenceDisplay.py?confId=4701>11th Pencil Code Meeting</a> will take place 11-15 May 2014 in Trondheim.
 Don't forget to sign up.
 </li>
 
