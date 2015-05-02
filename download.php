0f43f0691f102c60a933f8b9f6d00f8744e53045
Philippe Bourdin <git@Bourdin.ch>
2015-05-02 15:00:49 +0200

    Included password caching config.

--- download.php
+++ download.php
@@ -9,10 +9,14 @@ If you just want to check out the latest version of the code use:
 <div class="codescroll"><code>git clone https://github.com/pencil-code/pencil-code.git</code></div>
 
 If you have a github username 'MY_GIT_NAME' and like to submit changes you can use:
-<div class="codescroll"><code>git clone https://MY_GIT_NAME@github.com/pencil-code/pencil-code.git</code></div>
+<div class="codescroll"><code>git clone https://MY_GIT_NAME@github.com/pencil-code/pencil-code.git
+git config --global credential.helper 'cache --timeout=3600'</code></div>
+With the second line, you set the password cache to one hour.
 
+<p>
 Or, if you prefer to use a SSH keyfile so that you do not have to type your password on each 'push':
 <div class="codescroll"><code>git clone git@github.com:pencil-code/pencil-code.git</code></div>
+</p>
 
 The Pencil Code can also be obtained as a stable <a href="https://github.com/pencil-code/pencil-code/releases">release</a> (click <a href="http://norlx51.albanova.se/~brandenb/downloads/pencil-code-r21570.tar.gz">here</a> for a local copy r21570).
 </div>
