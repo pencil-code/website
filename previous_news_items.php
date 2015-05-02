111688a2982a6975ad71cd807f66efd6372efc42
Philippe Bourdin <git@Bourdin.ch>
2015-05-02 15:11:17 +0200

    Fixed typo.

--- previous_news_items.php
+++ previous_news_items.php
@@ -14,7 +14,7 @@ The <a href=http://agenda.albanova.se/conferenceDisplay.py?confId=4213>10th Penc
 Welcome to the new Pencil Code homepage, designed and implemented during the <a href="http://www.astro.lu.se/~michiel/PC2013/">9th Pencil Code User Meeting</a> on 17-20 June 2013 in Lund.</li>
 
 <li class="separated">21 Sep 2008:<br>
-The code was moved to <a href="http://pencil-code.googlecode.com/">Google Code repository</a>.</li>
+The code was moved to a <a href="http://pencil-code.googlecode.com/">Google Code repository</a>.</li>
 
 <li class="separated">28 July 2006:<br>
 All features of the eos branch have been incorporated into the main trunk.
