3d7afd4ea9b02e7f71e929c5d1696f8db5f52eb8
Philippe Bourdin <PABourdin@users.noreply.github.com>
2015-05-20 13:48:51 +0200

    Updated, added "SVN checkout", changed "SVN" to be recommended because of severe problems when using "GIT".

--- download.php
+++ download.php
@@ -4,12 +4,21 @@
  ?>
 <div class="centcolumnpad">
 <h2>Download</h2>
-We recommend getting the code with <em>git</em>.<br>
+You might get the code using <em>SVN</em> (recommended) or <em>GIT</em> (for git-experts).
+
+<h3>&bull; SVN checkout:</h3>
+The read-only version you can check out by:
+<div class="codescroll"><code>svn checkout https://github.com/pencil-code/pencil-code/trunk/ pencil-code</code></div>
+
+For a read-write access, you need to have a github user name ('MY_GITHUB_NAME') and become part of the <em>pencil-code</em> group.
+<div class="codescroll"><code>svn checkout https://github.com/pencil-code/pencil-code/trunk/ pencil-code --username 'MY_GITHUB_NAME'</code></div>
+
+<h3>&bull; GIT checkout:</h3>
 If you just want to check out the latest version of the code use:
 <div class="codescroll"><code>git clone https://github.com/pencil-code/pencil-code.git</code></div>
 
-If you have a github username 'MY_GIT_NAME' and like to submit changes you can use:
-<div class="codescroll"><code>git clone https://MY_GIT_NAME@github.com/pencil-code/pencil-code.git
+If you have a github username 'MY_GITHUB_NAME' and like to submit changes you can use:
+<div class="codescroll"><code>git clone https://MY_GITHUB_NAME@github.com/pencil-code/pencil-code.git
 git config --global credential.helper 'cache --timeout=3600'</code></div>
 With the second line, you set the password cache to one hour.
 
@@ -18,7 +27,9 @@ Or, if you prefer to use a SSH keyfile so that you do not have to type your pass
 <div class="codescroll"><code>git clone git@github.com:pencil-code/pencil-code.git</code></div>
 </p>
 
-The Pencil Code can also be obtained as a stable <a href="https://github.com/pencil-code/pencil-code/releases">release</a> (click <a href="http://norlx51.albanova.se/~brandenb/downloads/pencil-code-r21570.tar.gz">here</a> for a local copy r21570).
+<h3>&bull; Stable releases:</h3>
+The Pencil Code can also be obtained as a stable <a href="https://github.com/pencil-code/pencil-code/releases">release</a>.<br>
+We also provide the stable release <a href="http://norlx51.albanova.se/~brandenb/downloads/pencil-code-r21570.tar.gz">r21570 as TAR.gz archive</a> for direct download.
 </div>
 
 <div class="centerdivider"></div>
