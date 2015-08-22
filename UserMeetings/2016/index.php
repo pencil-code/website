5fdcc8dd32406f8ba60d4ae15d7283a49bdbd34e
Philippe Bourdin <git@Bourdin.ch>
2015-08-22 17:14:50 +0200

    Updated meeting information, will send a first notification to the list when this page is publicly visible.

--- UserMeetings/2016/index.php
+++ UserMeetings/2016/index.php
@@ -4,11 +4,36 @@
 <div class="centcolumnpad">
 <h2>Pencil Code User Meeting 2016</h2>
 
-The next meeting will be in Graz (Austria).<br>
-More details follow soon on this page...<br>
+<p>The next meeting will be in Graz (Austria).</p>
+
+<p>
+We are currently searching for the possible weeks, which will later undergo a poll.
+Until 19th of September, anyone may <a href="http://www.Bourdin.ch/Philippe/e-Mail.php">give me a short note</a>, when he/she is absent (or simply edit this page).
+<table border="0" cellpadding="5" cellspacing="5">
+<tr><td>date:</td><td>absent:</td></tr>
+<tr><td>23.5. - 27.5.</td><td></td></tr>
+<tr><td>30.5. - 03.6.</td><td></td></tr>
+<tr><td>06.6. - 10.6.</td><td>Axel</td></tr>
+<tr><td>13.6. - 17.6.</td><td></td></tr>
+<tr><td>27.6. - 01.7.</td><td></td></tr>
+<tr><td>04.7. - 08.7.</td><td></td></tr>
+<tr><td>11.7. - 15.7.</td><td></td></tr>
+<tr><td>18.7. - 22.7.</td><td></td></tr>
+<tr><td>25.7. - 29.7.</td><td></td></tr>
+<tr><td>01.8. - 05.8.</td><td></td></tr>
+<tr><td>08.8. - 12.8.</td><td></td></tr>
+</table>
+</p>
+
+<p>Currently, we plan for a 3-hours hike to &quot;B&auml;rensch&uuml;tzklamm&quot;, which requires some good hiking shoes and physical excercise, since we will &quot;climb&quot; up a waterfall via stairs and ladders (yes, it is pretty secure, because this is a well maintained trail). Furthermore, the social programm will comprise a conference dinner in a &quot;Buschenschank&quot;, where you might be offered some locally produces wines or juices and (mainly cold) food.</p>
+
+<p>The program of the meeting will be as usuas: scientific and technical presentation and discussions all around the Pencil Code.</p>
+
+<p>More details will follow on this page...<br>
 Thank you for visiting again.
 <!-- li><a href="http://agenda.albanova.se/conferenceDisplay.py?confId=4701">Pencil Code User Meeting</a> (meeting page)</li>
 <li><a href="/UserMeetings/2016/agenda.txt">agenda.txt</a></li -->
+</p>
 
 </div>
 <?php
