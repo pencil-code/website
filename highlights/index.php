6dde87b0f147c232d3579d3e3c717d4f62e76fe3
Philippe Bourdin <PABourdin@users.noreply.github.com>
2015-09-10 13:44:31 +0200

    Changed teaser image to fit size of other images on the highlings page.

--- highlights/index.php
+++ highlights/index.php
@@ -15,7 +15,7 @@ Currently, we may present the following highlights:
 	<tr valign="bottom">
 		<td align="center"><a href="/samples/turbulence/helical-MHDturb32-4procs/"><img src="/samples/turbulence/helical-MHDturb32-4procs/bz_thumb.jpg" width="150" height="125" border="0" alt="t=200-1700"></a></td>
 		<td align="center"><a href="/samples/ism/"><img src="/samples/ism/rho.jpg" width="150" height="125" border="0" alt="plasma density"></a></td> 
-		<td align="center"><a href="/samples/corona/"><img src="/samples/corona/corona_EUV-emission.png" width="150" height="150" border="0" alt="coronal loops EUV emission"></a></td>
+		<td align="center"><a href="/samples/corona/"><img src="/samples/corona/corona_EUV-emission.png" width="150" height="125" border="0" alt="coronal loops EUV emission"></a></td>
 	</tr>
 	<tr valign="top">
 		<td align="center"><a href="/samples/turbulence/helical-MHDturb32-4procs/">Helical MHD Dynamo</a> [AB]</td>
