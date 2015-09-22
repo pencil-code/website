9276b880bf99e1210840d18145d8cd69a8182b8e
Philippe Bourdin <PABourdin@users.noreply.github.com>
2015-09-22 19:33:56 +0200

    Updated.

--- UserMeetings/2016/index.php
+++ UserMeetings/2016/index.php
@@ -11,7 +11,7 @@ We are currently searching for the possible weeks, which will later undergo a po
 Until 19th of September, anyone may <a href="mailto:pc16@Bourdin.ch?subject=absence">give me a short note</a>, when he/she is absent (or simply edit this page).
 <table border="1" cellpadding="4" cellspacing="1">
 <tr><td>date:</td><td>absent:</td><td>conferences&amp;workshops:</td></tr>
-<tr><td>23.5. - 27.5.</td><td></td><td>AGU Chapman/Croatia</td></tr>
+<tr><td>23.5. - 27.5.</td><td>Philippe</td><td>AGU Chapman/Croatia</td></tr>
 <tr><td>30.5. - 03.6.</td><td>J&ouml;rn</td><td>SPD meeting/USA</td></tr>
 <tr><td>06.6. - 10.6.</td><td>Axel, J&ouml;rn</td><td>Cool Stars/Sweden</td></tr>
 <tr><td>13.6. - 17.6.</td><td></td><td>American Astronomical Society/USA</td></tr>
@@ -21,7 +21,7 @@ Until 19th of September, anyone may <a href="mailto:pc16@Bourdin.ch?subject=abse
 <tr><td>11.7. - 15.7.</td><td></td><td></td></tr>
 <tr><td>18.7. - 22.7.</td><td></td><td></td></tr>
 <tr><td>25.7. - 29.7.</td><td></td><td></td></tr>
-<tr><td>01.8. - 05.8.</td><td></td><td>COSPAR/Turkey</td></tr>
+<tr><td>01.8. - 05.8.</td><td>Jonas</td><td>COSPAR/Turkey, Int. Symp. on Combustion/Korea</td></tr>
 <tr><td>08.8. - 12.8.</td><td></td></td><td></tr>
 </table>
 </p>
