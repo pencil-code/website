c417b6dd21ec0922b95b33420a01d2b8f72c33b8
Philippe Bourdin <PABourdin@users.noreply.github.com>
2016-01-11 16:51:57 +0100

    Updated.

--- UserMeetings/2016/index.php
+++ UserMeetings/2016/index.php
@@ -7,8 +7,7 @@
 <p>The next meeting will be in <a href="https://en.wikipedia.org/wiki/Graz">Graz (Austria)</a> at the <a href="http://www.iwf.oeaw.ac.at/?L=1">Space Research Institute</a> of the <a href="http://www.oeaw.ac.at/en/">Austrian Academy of Sciences</a>.</p>
 
 <p>
-We are currently searching for the possible weeks, which will later undergo a poll.
-Until 19th of September, anyone may <a href="mailto:pc16@Bourdin.ch?subject=absence">give me a short note</a>, when he/she is absent (or simply edit this page).
+Anyone may <a href="mailto:pc16@Bourdin.ch?subject=absence">give me a short note</a>, when he/she is absent (or simply edit this page).
 <table border="1" cellpadding="4" cellspacing="1">
 <tr><td>date:</td><td>absent:</td><td>conferences&amp;workshops:</td></tr>
 <tr><td>23.5. - 27.5.</td><td>Philippe, Nils</td><td>AGU Chapman/Croatia, Multiphase Flow/Italy</td></tr>
