7e589aeef9b7e28225fd882e164f9bce9594fa68
Philippe Bourdin <PABourdin@users.noreply.github.com>
2016-02-16 16:17:36 +0100

    Updated meeting info.

--- UserMeetings/2016/index.php
+++ UserMeetings/2016/index.php
@@ -33,9 +33,9 @@ Anyone may <a href="mailto:pc16@Bourdin.ch?subject=absence">give me a short note
 
 <p>The program of the meeting will be as usual: Scientific presentations and technical discussions around the Pencil Code.</p>
 
-<p>If you plan to attend also the COSPAR assembly (until 7th of August) in Istanbul/Turkey, please note there are now daily (~2 hours direct) flights between Graz and Istanbul by <a href="http://www.turkishairlines.com/en-int/">Turkish Airlines</a>.</p>
-
 <p>Social program: There will be a ~3-hours hike to &quot;B&auml;rensch&uuml;tzklamm&quot;, which requires some good hiking shoes and physical excercise, since we will &quot;climb&quot; up a waterfall via stairs and ladders (yes, it is pretty secure, because this is a well maintained trail). Furthermore, the social program will comprise a conference dinner in a &quot;Buschenschank&quot;, where you might be offered some locally produced wines and juices and locally produced foods.</p>
+
+<p>Graz airport is now connected with direct flights to: Vienna, Frankfurt, Munich, Z&uuml;rich, D&uuml;sseldorf, Stuttgart, Berlin-Tegel, and Istanbul. If you plan to attend also the COSPAR assembly (until 7th of August) in Istanbul/Turkey, please note there are now daily (~2 hours direct) flights between Graz and Istanbul by <a href="http://www.turkishairlines.com/en-int/">Turkish Airlines</a>.</p>
 
 <p>More details will follow on this page...<br>
 Thank you for visiting again.
