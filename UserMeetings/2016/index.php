9ccb1fc04d2a52093c2e7fd032a5b0a06078de5d
Philippe Bourdin <PABourdin@users.noreply.github.com>
2016-02-16 16:07:31 +0100

    Updated 2016 meeting information.

--- UserMeetings/2016/index.php
+++ UserMeetings/2016/index.php
@@ -7,6 +7,7 @@
 <p>The next meeting will be in <a href="https://en.wikipedia.org/wiki/Graz">Graz (Austria)</a> at the <a href="http://www.iwf.oeaw.ac.at/?L=1">Space Research Institute</a> of the <a href="http://www.oeaw.ac.at/en/">Austrian Academy of Sciences</a>.</p>
 
 <p>
+<!-- 
 Anyone may <a href="mailto:pc16@Bourdin.ch?subject=absence">give me a short note</a>, when he/she is absent (or simply edit this page).
 <table border="1" cellpadding="4" cellspacing="1">
 <tr><td>date:</td><td>absent:</td><td>conferences&amp;workshops:</td></tr>
@@ -21,13 +22,20 @@ Anyone may <a href="mailto:pc16@Bourdin.ch?subject=absence">give me a short note
 <tr><td>18.7. - 22.7.</td><td>Nils</td><td></td></tr>
 <tr><td>25.7. - 29.7.</td><td></td><td></td></tr>
 <tr><td>01.8. - 05.8.</td><td>Jonas, Nils</td><td>COSPAR/Turkey, Combustion Symposium/Korea</td></tr>
-<tr><td>08.8. - 12.8.</td><td><font color="#E00000">CANDIDATE WEEK</font></td></td><td></tr>
+</table>
+ -->
+<table border="1" cellpadding="4" cellspacing="1">
+<tr><td>date:</td><td>conferences&amp;workshops:</td></tr>
+<tr><td>31.7. - 07.8.</td>><td>COSPAR/Turkey, Combustion Symposium/Korea</td></tr>
+<tr><td>08.8. - 12.8.</td><td><font color="#E00000">CHOSEN WEEK</font></td></tr>
 </table>
 </p>
 
-<p>Currently, we plan for a 3-hours hike to &quot;B&auml;rensch&uuml;tzklamm&quot;, which requires some good hiking shoes and physical excercise, since we will &quot;climb&quot; up a waterfall via stairs and ladders (yes, it is pretty secure, because this is a well maintained trail). Furthermore, the social program will comprise a conference dinner in a &quot;Buschenschank&quot;, where you might be offered some locally produced wines and juices and local food.</p>
+<p>The program of the meeting will be as usual: Scientific presentations and technical discussions around the Pencil Code.</p>
 
-<p>The program of the meeting will be as usual: scientific presentations and technical discussions around the Pencil Code.</p>
+<p>If you plan to attend also the COSPAR assembly (until 7th of August) in Istanbul/Turkey, please note there are now daily (~2 hours direct) flights between Graz and Istanbul by <a href="http://www.turkishairlines.com/en-int/">Turkish Airlines</a>.</p>
+
+<p>Social program: There will be a ~3-hours hike to &quot;B&auml;rensch&uuml;tzklamm&quot;, which requires some good hiking shoes and physical excercise, since we will &quot;climb&quot; up a waterfall via stairs and ladders (yes, it is pretty secure, because this is a well maintained trail). Furthermore, the social program will comprise a conference dinner in a &quot;Buschenschank&quot;, where you might be offered some locally produced wines and juices and locally produced foods.</p>
 
 <p>More details will follow on this page...<br>
 Thank you for visiting again.
