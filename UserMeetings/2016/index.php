0982e49d5cf5a0c467bf06454ecb7b043625d5b8
Philippe Bourdin <PABourdin@users.noreply.github.com>
2016-02-25 13:02:29 +0100

    Added link to agneda.txt.

--- UserMeetings/2016/index.php
+++ UserMeetings/2016/index.php
@@ -32,6 +32,7 @@ Anyone may <a href="mailto:pc16@Bourdin.ch?subject=absence">give me a short note
 </p>
 
 <p>The program of the meeting will be as usual: Scientific presentations and technical discussions around the Pencil Code.</p>
+<p>Please find here the <a href="/UserMeetings/2016/agenda.txt">agenda.txt</a>.</p>
 
 <p>Social program: There will be a ~3-hours hike to &quot;B&auml;rensch&uuml;tzklamm&quot;, which requires some good hiking shoes and physical excercise, since we will &quot;climb&quot; up a waterfall via stairs and ladders (yes, it is pretty secure, because this is a well maintained trail). Furthermore, the social program will comprise a conference dinner in a &quot;Buschenschank&quot;, where you might be offered some locally produced wines and juices and locally produced foods.</p>
 
@@ -39,8 +40,7 @@ Anyone may <a href="mailto:pc16@Bourdin.ch?subject=absence">give me a short note
 
 <p>More details will follow on this page...<br>
 Thank you for visiting again.
-<!-- li><a href="http://agenda.albanova.se/conferenceDisplay.py?confId=4701">Pencil Code User Meeting</a> (meeting page)</li>
-<li><a href="/UserMeetings/2016/agenda.txt">agenda.txt</a></li -->
+<!-- li><a href="http://agenda.albanova.se/conferenceDisplay.py?confId=4701">Pencil Code User Meeting</a> (meeting page)</li -->
 </p>
 
 </div>
