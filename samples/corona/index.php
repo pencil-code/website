287707a930eca8fd4ecdf4213324ed45dba14c62
Philippe Bourdin <PABourdin@users.noreply.github.com>
2016-05-31 16:37:17 +0200

    Added one more publication.

--- samples/corona/index.php
+++ samples/corona/index.php
@@ -33,6 +33,7 @@ References:
 	<li>Bourdin et al. (2013), A&amp;A 555, A123 (<a href="http://adsabs.harvard.edu/abs/2013A%26A...555A.123B">ADS</a>, DOI: <a href="http://dx.doi.org/10.1051/0004-6361/201321185">10.1051/0004-6361/201321185</a>)</li>
 	<li>Bourdin et al. (2014), PASJ 66, S7&nbsp;&nbsp;&nbsp;&nbsp; (<a href="http://adsabs.harvard.edu/abs/2014PASJ...66S...7B">ADS</a>, DOI: <a href="http://dx.doi.org/10.1093/pasj/psu123">10.1093/pasj/psu123</a>)</li>
 	<li>Bourdin et al. (2015), A&amp;A 580, A72&nbsp;&nbsp; (<a href="http://adsabs.harvard.edu/abs/2015A%26A...580A..72B">ADS</a>, DOI: <a href="http://dx.doi.org/10.1051/0004-6361/201525839">10.1051/0004-6361/201525839</a>)</li>
+	<li>Bourdin et al. (2016), A&amp;A 589, A86&nbsp;&nbsp; (<a href="http://adsabs.harvard.edu/abs/2016A%26A...589A..86B">ADS</a>, DOI: <a href="http://dx.doi.org/10.1051/0004-6361/201525840">10.1051/0004-6361/201525840</a>)</li>
 </ul>
 
 </div>
