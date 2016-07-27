3395b79f211d0332e686a3a046adfa5d7ecb2dfd
Philippe Bourdin <PABourdin@users.noreply.github.com>
2016-07-27 14:21:43 +0200

    Added detailed schedule of meeting.

--- UserMeetings/2016/index.php
+++ UserMeetings/2016/index.php
@@ -20,23 +20,57 @@ The meeting will be in <a href="https://en.wikipedia.org/wiki/Graz">Graz (Austri
 <!-- PROGRAM -->
 <p><b><u>PROGRAM:</u></b><br>
 The program of the meeting will be as usual: Scientific presentations and technical discussions around the Pencil Code.<br>
-Please find here the <a href="/UserMeetings/2016/agenda.txt">agenda.txt</a> and a preliminary program:<br>
+Please find here the <a href="/UserMeetings/2016/agenda.txt">agenda.txt</a> and our detailed schedule:<br>
 <table border="1" cellpadding="4" cellspacing="1">
-<tr><td rowspan="4" bgcolor="#f0f0f0">Monday</td><td>morning</td><td>meet &amp; greet</td></tr>
-<tr><td>13:30</td><td>Official Welcome</td></tr>
-<tr><td>afternoon</td><td bgcolor="#f0f080">status reports &amp; discussions</td></tr>
-<tr><td>evening</td><td>city walking tour &amp; dinner</td></tr>
-<tr><td rowspan="2" bgcolor="#c0c0c0">Tuesday</td><td>morning</td><td bgcolor="#f0c040">presentations &amp; discussions</td></tr>
-<tr><td>afternoon</td><td>excursion to &quot;B&auml;rensch&uuml;tzklamm&quot;</td></tr>
-<tr><td rowspan="3" bgcolor="#f0f0f0">Wednesday</td><td>morning</td><td bgcolor="#f0c040">presentations &amp; discussions</td></tr>
-<tr><td>afternoon</td><td bgcolor="#95e080">general methods &amp; discussions</td></tr>
-<tr><td>evening</td><td>barbecue at the institute</td></tr>
-<tr><td rowspan="3" bgcolor="#c0c0c0">Thursday</td><td>morning</td><td bgcolor="#f0c040">presentations &amp; discussions</td></tr>
-<tr><td>afternoon</td><td bgcolor="#95e080">general code questions &amp; discussions</td></tr>
-<tr><td>evening</td><td>&quot;Buschenschank&quot; dinner</td></tr>
-<tr><td rowspan="3" bgcolor="#f0f0f0">Friday</td><td>morning</td><td bgcolor="#a0f0ff">expert coding &amp; beginners hands-on sessions</td></tr>
-<tr><td>afternoon</td><td bgcolor="#a0f0ff">expert coding &amp; beginners hands-on sessions</td></tr>
-<tr><td>evening</td><td>formal end of meeting</td></tr>
+
+<tr><td rowspan="6" bgcolor="#f0f0f0">Monday, 8</td>
+    <td>morning</td><td>meet &amp; greet</td></tr>
+<tr bgcolor="#f0a080"><td>13:30</td><td>Official Welcome</td></tr>
+<tr><td>13:45</td><td>Axel: code updates &amp; statistics</td></tr>
+<tr><td>14:15</td><td>discussions on agenda</td></tr>
+<tr><td>15:00</td><td>status reports</td></tr>
+<tr bgcolor="#f0a080"><td>17:30</td><td>city waling tour &amp; dinner</td></tr>
+
+<tr><td rowspan="6" bgcolor="#c0c0c0">Tuesday, 9</td>
+    <td>09:00</td><td>Jonas: Combusting particles in the Pencil Code</td></tr>
+<tr><td>09:30</td><td>Nils: The effect of turbulence on particles with surface reactions</td></tr>
+<tr bgcolor="#f0a080"><td>10:00</td><td>coffee break</td></tr>
+<tr><td>10:30</td><td>J&oslash;rgen: The effect of free-stream turbulence on the transition in the wake of a circular cylinder</td></tr>
+<tr><td>11:00</td><td>discussions related to particles</td></tr>
+<tr bgcolor="#f0a080"><td>12:00</td><td>sandwich lunch &amp; excursion</td></tr>
+
+<tr><td rowspan="13" bgcolor="#f0f0f0">Wednesday, 10</td>
+    <td>09:00</td><td>Akshay: Relative velocities and collisions between heavy particles in turbulent flows</td></tr>
+<tr><td>09:30</td><td>Kiwan: Inverse transfer of magnetic energy in decaying MHD turbulence</td></tr>
+<tr bgcolor="#f0a080"><td>10:00</td><td>discussions &amp; coffee break</td></tr>
+<tr><td>11:00</td><td>Rohit: Energisation of particles in magnetic reconnection</td></tr>
+<tr><td>11:30</td><td>Philippe: The PIC method and its application to mangetic reconnection</td></tr>
+<tr bgcolor="#f0a080"><td>12:00</td><td>discussions &amp; lunch break</td></tr>
+<tr><td>13:30</td><td>J&ouml;rn: Slope limited diffusion in the Pencil Code</td></tr>
+<tr><td>14:00</td><td>Matthias: PC goes YY</td></tr>
+<tr><td>14:30</td><td>Wlad: Multiple special modules</td></tr>
+<tr bgcolor="#f0a080"><td>15:00</td><td>discussions &amp; coffee break</td></tr>
+<tr><td>15:30</td><td>Alex: A Barnes-Hut self-gravity module for Pencil</td></tr>
+<tr><td>16:00</td><td>flexible discussions</td></tr>
+<tr bgcolor="#f0a080"><td>17:30</td><td>barbequeue</td></tr>
+
+<tr><td rowspan="11" bgcolor="#c0c0c0">Thursday, 11</td>
+    <td>09:00</td><td>Fred: Test field application &amp; issues related to timestep control</td></tr>
+<tr><td>09:30</td><td>Edward &amp; Navin: Explosive magneto-rotational instability</td></tr>
+<tr bgcolor="#f0a080"><td>10:00</td><td>discussions &amp; coffee break</td></tr>
+<tr><td>11:00</td><td>Graeme: Cosmic Rays in the Interstellar Medium</td></tr>
+<tr><td>11:30</td><td>Dhruba: Lyapunov exponents and the kinematic dynamo</td></tr>
+<tr bgcolor="#f0a080"><td>12:00</td><td>discussions &amp; lunch break</td></tr>
+<tr><td>13:30</td><td>general code questions &amp; discussions</td></tr>
+<tr><td>14:00</td><td>Axel: The role of magnetic helicity in solar physics (Institute's colloquium)</td></tr>
+<tr bgcolor="#f0a080"><td>15:00</td><td>discussions &amp; coffee break</td></tr>
+<tr><td>16:00</td><td>summaries &amp; outlook</td></tr>
+<tr bgcolor="#f0a080"><td>18:00</td><td>&quot;Buschenschank&quot; dinner</td></tr>
+
+<tr><td rowspan="3" bgcolor="#f0f0f0">Friday, 12</td>
+    <td>09:00</td><td>expert coding &amp; beginners hands-on sessions</td></tr>
+<tr bgcolor="#f0a080"><td>12:30</td><td>lunch break</td></tr>
+<tr><td>13:30</td><td>expert coding &amp; beginners hands-on sessions</td></tr>
 </table>
 </p>
 
