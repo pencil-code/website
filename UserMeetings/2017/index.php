1fe4e37280ed598d0c5949166e35b0b862b3daf9
Philippe Bourdin <PABourdin@users.noreply.github.com>
2016-10-23 17:13:14 +0200

    Added 2017 meeting page.

---
+++ UserMeetings/2017/index.php
@@ -0,0 +1,13 @@
+<?php
+	set_include_path ("../../");
+	include "inc/header.inc";
+ ?>
+<div class="centcolumnpad">
+<h2>13<sup>th</sup> Pencil Code User Meeting 2017</h2>
+
+<li><a href="/UserMeetings/2017/agenda.txt">agenda.txt</a></li>
+
+</div>
+<?php
+	include "inc/footer.inc";
+ ?>
--- meeting2016.php
+++
@@ -1,4 +0,0 @@
-<?php
-	# forward request to meeting page:
-	header ("Location: http://pencil-code.nordita.org/UserMeetings/2016/");
-?>
