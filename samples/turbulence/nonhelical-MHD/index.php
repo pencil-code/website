f4b26b51cf7e7a0c617717dd08d0a36ba23c1a31
Philippe Bourdin <PABourdin@users.noreply.github.com>
2016-12-13 12:34:15 +0100

    Fixed broken image links.

--- samples/turbulence/nonhelical-MHD/index.php
+++ samples/turbulence/nonhelical-MHD/index.php
@@ -3,7 +3,7 @@
 	 include "inc/header.inc";
  ?>
 <div class="centcolumnpad">
-<img align="right" src="/samples/turbulence/pslice.png" width="250" height="200" border="1" hspace="20" >
+<img align="right" src="pslice.png" width="250" height="200" border="1" hspace="20" >
 
 Contours of (a) <i>B<sub>z</sub>(x,y)</i> and (b) <i>u<sub>z</sub>(x,y)</i>.
 The insets show a zoom into the small square in the lower left corner.
@@ -13,8 +13,8 @@ The insets show a zoom into the small square in the lower left corner.
 
 In the presence of magnetic helicity, inverse transfer from small to large scales is well known in magnetohydrodynamic (MHD) turbulence and has applications in astrophysics, cosmology, and fusion plasmas. Using high resolution direct numerical simulations of magnetically dominated self-similarly decaying MHD turbulence, we report a similar inverse transfer even in the absence of magnetic helicity. We compute for the first time spectral energy transfer rates to show that this inverse transfer is about half as strong as with helicity, but in both cases the magnetic gain at large scales results from velocity at similar scales interacting with smaller-scale magnetic fields. This suggests that both inverse transfers are a consequence of a universal mechanisms for magnetically dominated turbulence. Possible explanations include inverse cascading of the mean squared vector potential associated with local near two-dimensionality and the shallower k^2 subinertial range spectrum of kinetic energy forcing the magnetic field with a k^4 subinertial range to attain larger-scale coherence. The inertial range shows a clear k^{-2} spectrum and is the first example of fully isotropic magnetically dominated MHD turbulence exhibiting weak turbulence scaling.
 
-<img align="right" src="/samples/turbulence/pkt2304_hel_short2304pm1_kf60b_noforce.png" width="250" height="200" border="1" hspace="20" >
-<img align="right" src="/samples/turbulence/pkt2304_hel_short2304pm100_kf60b_noforce.png" width="250" height="200" border="1" hspace="20" >
+<img align="right" src="pkt2304_hel_short2304pm1_kf60b_noforce.png" width="250" height="200" border="1" hspace="20" >
+<img align="right" src="pkt2304_hel_short2304pm100_kf60b_noforce.png" width="250" height="200" border="1" hspace="20" >
 
 (a) Magnetic (solid lines) and kinetic (dashed lines) energy
 spectra for Run~A at times t/&tau;<sub>A</sub>=18, 130, 450, and 1800;
