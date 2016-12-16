cc0aedac115ce35ac2b35b42aaa980ddca860f5c
Axel Brandenburg <brandenb@nordita.org>
2016-12-15 23:31:01 -0700

    updates

--- contact.php
+++ contact.php
@@ -5,6 +5,9 @@
 <div class="centcolumnpad">
 <h2>Contact</h2>
 Detailed questions regarding the code and its usage can be discussed on our <a href="https://groups.google.com/forum/?fromgroups#!forum/pencil-code-discuss">public&nbsp;mailing&nbsp;list</a>.
+
+Issues of major concern to the Pencil Code community should be addressed
+to the <a href=https://www.nordita.org/~brandenb/pencil-code/PCSC/>Pencil Code Steering Committee</a>.
 </div>
 
 <div class="centerdivider"></div>
@@ -19,9 +22,12 @@ The current core-development team consists of:
 <li>Nils E. Haugen</li>
 <li>Anders Johansen</li>
 <li>Petri K&auml;pyl&auml;</li>
+<li>Maarit K&auml;pyl&auml;</li>
 <li>Wladimir Lyra</li>
 <li>Dhruba Mitra</li>
 <li>Matthias Rheinhardt</li>
+<li>J&ouml;rn Warnecke</li>
+<li>Chao-Chin Yang</li>
 </ul>
 
 All the people involved in this project are <a href="https://github.com/pencil-code/pencil-code/graphs/contributors">listed here</a>.
