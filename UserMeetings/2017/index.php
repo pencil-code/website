56bfec07b020908306276b0cf5b290b311e00724
Graeme R Sarson <g.r.sarson@ncl.ac.uk>
2017-01-23 12:19:57 +0000

    typo

--- UserMeetings/2017/index.php
+++ UserMeetings/2017/index.php
@@ -9,12 +9,12 @@
 <!-- DATE and VENUE -->
 <p><b><u>DATE &amp; VENUE:</u></b>
 <b>10<sup>th</sup> to 14<sup>th</sup> July 2017</b><br>
-The meeting was held in <a href="https://en.wikipedia.org/wiki/Newcastle_upon_Tyne">Newcastle (UK)</a> in the <a href="http://www.ncl.ac.uk/maths/">School of Mathematics & Statistics</a> of <a href="http://www.ncl.ac.uk/">Newcastle University</a>.
+The meeting will be held in <a href="https://en.wikipedia.org/wiki/Newcastle_upon_Tyne">Newcastle (UK)</a> in the <a href="http://www.ncl.ac.uk/maths/">School of Mathematics & Statistics</a> of <a href="http://www.ncl.ac.uk/">Newcastle University</a>.
 </p>
 
 <!-- PROGRAM -->
 <p><b><u>PROGRAM:</u></b><br>
-Details of the meeting will be arranged in due course.
+Details of the meeting will be posted in due course.
 For now, please note requested discussion topics in <a href="/UserMeetings/2017/agenda.txt">agenda.txt</a>.
 
 <!--
