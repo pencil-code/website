dca7bdc380c406a718cfadb7824c07a99aceb07a
Graeme R Sarson <g.r.sarson@ncl.ac.uk>
2017-05-04 12:53:27 +0100

    add travel info.

--- UserMeetings/2017/index.php
+++ UserMeetings/2017/index.php
@@ -17,6 +17,48 @@ The meeting will be held in <a href="https://en.wikipedia.org/wiki/Newcastle_upo
 Details of the meeting will be posted in due course.
 For now, please note requested discussion topics in <a href="/UserMeetings/2017/agenda.txt">agenda.txt</a>.
 
+<!-- TRAVEL -->
+<p><b><u>TRAVEL:</u></b><br>
+The meeting will take place in the 
+<a href="http://www.ncl.ac.uk/tour/academic/herschel-building/">Herschel Building</a>,
+the home of the <a href="http://www.ncl.ac.uk/maths/">School of Mathematics &amp; Statistics</a> 
+of 
+<a href="http://www.ncl.ac.uk/">Newcastle University</a>.
+The Herschel Building is opposite the Haymarket Metro station,
+in Newcastle City Centre.
+</p>
+
+<p>
+Details about getting to Newcastle and reaching the University can be found 
+on the 
+<a href="http://www.ncl.ac.uk/maths/about/finding_us.htm">School</a>
+and 
+<a href="http://www.ncl.ac.uk/about/visit/travel/">University</a>
+travel pages.
+A variety of 
+<a href="http://www.ncl.ac.uk/about/visit/maps.htm">University maps</a>
+are also available.
+(Perhaps most useful is the
+<a href="http://www.ncl.ac.uk/media/wwwnclacuk/abouttheuniversity/files/campus-map.pdf">Campus map</a>;
+the Herschel Building is Building 17 on this map.)
+</p>
+
+<p>
+Other useful resources are available on the pages of the
+<a href="http://www.newcastlegateshead.com/">Newcastle Gateshead Initiative</a>.
+These include additional
+<a href="http://www.newcastlegateshead.com/plan-your-visit/travel">Travel</a>
+and
+<a href="http://www.newcastlegateshead.com/plan-your-visit/maps">Map</a>
+pages; and their
+<a href="http://www.newcastlegateshead.com/plan-your-visit/travel/getting-around">Getting around</a>
+page is a good introduction to the city's public transport options.
+</p>
+
+<!-- ACCOMMODATION -->
+
+Accommodation options will be posted here shortly.
+
 <!--
 <li><a href="/UserMeetings/2017/agenda.txt">agenda.txt</a></li>
 -->
