7f24a00c9311e4d36f5441ecec3c3091e78ca726
Axel Brandenburg <brandenb@nordita.org>
2021-05-21 06:25:46 +0200

    added link to office hour page

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@
 <div class="centcolumnpad">
 <h2>Contact</h2>
 <h3>Pencil Code Office Hours</h3>
-Please make use of the Pencil Code Office Hours every second Friday
+Please make use of the Pencil Code Office Hours every <a href=https://www.nordita.org/~brandenb/pencil-code/office_hours/>second Friday</a>
 of the month at 14:30 Central European Time (CET) or Central European
 Summer Time (CEST, in the Summer) to chat with the experts informally on
 <a href="https://stockholmuniversity.zoom.us/j/6415995185">zoom:6415995185</a>.<BR>
