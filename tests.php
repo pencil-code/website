f9f3eca4609471aaff76ce13f6ed23fb9368bb68
Philippe Bourdin <11717935+PABourdin@users.noreply.github.com>
2023-01-17 16:19:24 +0100

    Updated Axel's compiler information.

--- tests.php
+++ tests.php
@@ -20,7 +20,7 @@ our own tests. The latest test results for a can be seen online:</p>
 <li><a href="http://www.travis-ci.com/pencil-code/pencil-code">Travis</a>, after each check-in
 -->
 <li><a href="https://app.travis-ci.com/github/pencil-code/pencil-code">Travis</a>, after each check-in
-<li><a href="http://norlx51.nordita.org/~brandenb/pencil-code/tests/hourly-samples/">Nordita: GNU gfortran 4.6.3-1ubuntu5 with OpenMPI (norlx51, by Wolfgang/Axel)</a>, hourly (<a href="http://norlx51.nordita.org/~brandenb/pencil-code/tests/hourly-samples/previous.html">previous run</a>)
+<li><a href="http://norlx51.nordita.org/~brandenb/pencil-code/tests/hourly-samples/">Nordita: GNU gfortran 6.4.0 with OpenMPI (norlx51, by Wolfgang/Axel)</a>, hourly (<a href="http://norlx51.nordita.org/~brandenb/pencil-code/tests/hourly-samples/previous.html">previous run</a>)
 <!-- Daily test -->
 <li><a href="http://norlx51.nordita.org/~brandenb/pencil-code/tests/daily-samples/">Nordita: g95 0.92 with OpenMPI (norlx51, by Wolfgang/Axel)</a>, daily
 <!-- Philippe Bourdin -->
