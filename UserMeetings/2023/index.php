7193c368b5d99d13b62c250d11621a270fc5bc02
Philippe Bourdin <11717935+PABourdin@users.noreply.github.com>
2023-02-20 14:54:55 +0100

    Updated information on next user meeting 2023 in Graz.

---
+++ UserMeetings/2023/index.php
@@ -0,0 +1,96 @@
+<?php
+	set_include_path ("../../");
+	include "inc/header.inc";
+ ?>
+<div class="centcolumnpad">
+<!-- HEADER -->
+<h2>12<sup>th</sup> Pencil Code User Meeting 2023</h2>
+
+<!-- DATE and VENUE -->
+<p><b><u>DATE &amp; VENUE:</u></b>
+<b>4<sup>th</sup> to 8<sup>th</sup> September 2023</b><br>
+The meeting was held in <a href="https://en.wikipedia.org/wiki/Graz">Graz (Austria)</a> at the <a href="https://physik.uni-graz.at/en/igam/">Department for Geo-, Astrophysics, and Meteorology (IGAM)</a> of the <a href="https://uni-graz.at/en/">University of Graz</a>.
+</p>
+
+<!-- PROGRAM -->
+<p><b><u>PROGRAM:</u></b><br>
+The program of the meeting will be as usual: Scientific presentations and technical discussions around the Pencil Code.<br>
+Please find here the <a href="/UserMeetings/2023/agenda.txt">agenda.txt</a>
+<!-- and our detailed schedule:<br>
+<table border="1" cellpadding="4" cellspacing="1">
+
+<tr><td rowspan="6" bgcolor="#f0f0f0">Monday, 8</td>
+    <td>morning</td><td>meet &amp; greet</td></tr>
+<tr bgcolor="#f0a080"><td>13:30</td><td>Official Welcome</td></tr>
+<tr><td>13:45</td><td>Axel: code updates &amp; statistics</td></tr>
+<tr><td>14:15</td><td>discussions on agenda</td></tr>
+<tr><td>15:00</td><td>status reports</td></tr>
+<tr bgcolor="#f0a080"><td>17:30</td><td>city waling tour &amp; dinner</td></tr>
+
+<tr><td rowspan="6" bgcolor="#c0c0c0">Tuesday, 9</td>
+    <td>09:00</td><td>Jonas Kr&uuml;ger: Combusting particles in the Pencil Code (via internet)</td></tr>
+<tr><td>09:30</td><td>Nils: The effect of turbulence on particles with surface reactions</td></tr>
+<tr bgcolor="#f0a080"><td>10:00</td><td>coffee break</td></tr>
+<tr><td>10:30</td><td>J&oslash;rgen: The effect of free-stream turbulence on the transition in the wake of a circular cylinder</td></tr>
+<tr><td>11:00</td><td>discussions related to particles</td></tr>
+<tr bgcolor="#f0a080"><td>12:00</td><td>sandwich lunch &amp; excursion to &quot;B&auml;rensch&uuml;tzklamm&quot;</td></tr>
+
+<tr><td rowspan="14" bgcolor="#f0f0f0">Wednesday,&nbsp;10</td>
+    <td>09:00</td><td>Akshay: Relative velocities and collisions between heavy particles in turbulent flows</td></tr>
+<tr><td>09:30</td><td>Kiwan: Inverse transfer of magnetic energy in decaying MHD turbulence</td></tr>
+<tr bgcolor="#f0a080"><td>10:00</td><td>discussions &amp; coffee break</td></tr>
+<tr><td>11:00</td><td>Rohit: Energisation of particles in magnetic reconnection</td></tr>
+<tr><td>11:30</td><td>Philippe: The PIC method and its application to mangetic reconnection</td></tr>
+<tr bgcolor="#f0a080"><td>12:00</td><td>discussions &amp; lunch break</td></tr>
+<tr><td>13:30</td><td>J&ouml;rn: Slope limited diffusion in the Pencil Code</td></tr>
+<tr><td>14:00</td><td><a href="/UserMeetings/2023/talks/YinYang_talk.pdf">Matthias: PC goes YY</a></td></tr>
+<tr><td>14:30</td><td>Wlad: Multiple special modules</td></tr>
+<tr bgcolor="#f0a080"><td>15:00</td><td>discussions &amp; coffee break</td></tr>
+<tr><td>15:30</td><td>Alex: A Barnes-Hut self-gravity module for Pencil</td></tr>
+<tr><td>16:00</td><td>Tobias: GIT introduction &amp; discussions</td></tr>
+<tr><td>16:30</td><td>flexible discussions</td></tr>
+<tr bgcolor="#f0a080"><td>17:30</td><td>barbequeue</td></tr>
+
+<tr><td rowspan="11" bgcolor="#c0c0c0">Thursday, 11</td>
+    <td>09:00</td><td>Fred: Test field application &amp; issues related to timestep control</td></tr>
+<tr><td>09:30</td><td>Edward &amp; Navin: Explosive magneto-rotational instability</td></tr>
+<tr bgcolor="#f0a080"><td>10:00</td><td>discussions &amp; coffee break</td></tr>
+<tr><td>11:00</td><td>Graeme: Cosmic Rays in the Interstellar Medium</td></tr>
+<tr><td>11:30</td><td>Dhruba: Lyapunov exponents and the kinematic dynamo</td></tr>
+<tr bgcolor="#f0a080"><td>12:00</td><td>discussions &amp; lunch break</td></tr>
+<tr><td>13:30</td><td>general code questions &amp; discussions</td></tr>
+<tr><td>14:00</td><td>Axel: The role of magnetic helicity in solar physics (Institute's colloquium)</td></tr>
+<tr bgcolor="#f0a080"><td>15:00</td><td>discussions &amp; coffee break</td></tr>
+<tr><td>16:00</td><td>summaries &amp; outlook</td></tr>
+<tr bgcolor="#f0a080"><td>18:00</td><td>&quot;Buschenschank&quot; dinner</td></tr>
+
+<tr><td rowspan="3" bgcolor="#f0f0f0">Friday, 12</td>
+    <td>09:00</td><td>expert coding &amp; beginners hands-on sessions</td></tr>
+<tr bgcolor="#f0a080"><td>12:30</td><td>lunch break</td></tr>
+<tr><td>13:30</td><td>expert coding &amp; beginners hands-on sessions</td></tr>
+</table>
+</p>
+ -->
+
+<!-- PARTICIPANTS -->
+<p><b><u>PARTICIPANTS:</u></b>
+<ul>
+<li>Vartika Pandey, IGAM/Graz</li>
+<li>Johannes Tschernitz, IGAM/Graz</li>
+<li>Isabella Kraus, IGAM/Graz</li>
+<li>Philippe-A. Bourdin, IGAM/Graz</li>
+<li>Axel Brandenburg, NORDITA/Stockholm</li>
+<!-- li>Frederick Gent, Aalto University</li -->
+<!-- li>Nils Haugen, NTNU/SINTEF Trondheim</li -->
+<!-- li>Wladimir Lyra, JPL/NASA Pasadena</li -->
+<!-- li>Dhrubaditya Mitra, NORDITA/Stockholm</li -->
+<!-- li>Matthias Rheinhardt, University of Helsinki</li -->
+<!-- li>J&ouml;rn Warnecke, MPS G&ouml;ttingen</li -->
+</ul></p>
+
+<!-- p>The next meeting will be in (t.b.c.)...</p -->
+
+</div>
+<?php
+	include "inc/footer.inc";
+ ?>
