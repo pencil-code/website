77d1d7fcf990151a334660a5d47c6d641ddccc64
Philippe Bourdin <11717935+PABourdin@users.noreply.github.com>
2023-03-08 18:25:50 +0100

    Added thank-you page after the registration form.

---
+++ UserMeetings/2023/thanks.php
@@ -0,0 +1,19 @@
+<?php
+    set_include_path ("../../");
+    include "inc/header.inc";
+ ?>
+<div class="centcolumnpad">
+<!-- HEADER -->
+<h2>19<sup>th</sup> Pencil Code User Meeting 2023</h2>
+
+<!-- REGISTRATION -->
+<p><b><u>REGISTRATION:</u></b><br>
+<font color="#f00000"><b>Thank you!</b></font> Your registration was submitted.<br>
+</p>
+
+<p>You may now proceed to our <a href="./">2023 meeting website</a>...</p>
+
+</div>
+<?php
+    include "inc/footer.inc";
+ ?>
