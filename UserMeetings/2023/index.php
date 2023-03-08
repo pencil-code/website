03ce4aa6268d77c4afa48df1f21079e00849a2f8
Philippe Bourdin <11717935+PABourdin@users.noreply.github.com>
2023-03-08 18:46:39 +0100

    Wording.

--- UserMeetings/2023/index.php
+++ UserMeetings/2023/index.php
@@ -25,10 +25,10 @@ Please find here the <a href="/UserMeetings/2023/agenda.txt">agenda.txt</a> and 
     <td style="min-width:130px">Friday</td></tr>
 <tr><td>09:00-09:30</td>
     <td rowspan="9" bgcolor="#ddffa1">Meet &amp; Greet</td>
-    <td rowspan="3" bgcolor="#b9f8fa">Session 2.1<br>Talk &amp; Discussion</td>
-    <td rowspan="3" bgcolor="#b9f8fa">Session 3.1<br>Talk &amp; Discussion</td>
-    <td rowspan="3" bgcolor="#b9f8fa">Session 4.1<br>Talk &amp; Discussion</td>
-    <td rowspan="3" bgcolor="#b9f8fa">Session 5.1<br>Talk &amp; Discussion</td></tr>
+    <td rowspan="3" bgcolor="#b9f8fa">Session 2.1<br>Talks &amp; Discussion</td>
+    <td rowspan="3" bgcolor="#b9f8fa">Session 3.1<br>Talks &amp; Discussion</td>
+    <td rowspan="3" bgcolor="#b9f8fa">Session 4.1<br>Talks &amp; Discussion</td>
+    <td rowspan="3" bgcolor="#b9f8fa">Session 5.1<br>Talks &amp; Discussion</td></tr>
 <tr><td>09:30-10:00</td></tr>
 <tr><td>10:00-10:30</td></tr>
 <tr><td>10:30-11:00</td>
@@ -37,10 +37,10 @@ Please find here the <a href="/UserMeetings/2023/agenda.txt">agenda.txt</a> and 
     <td rowspan="1" bgcolor="#f0a080">Coffee Break</td>
     <td rowspan="1" bgcolor="#f0a080">Coffee Break</td></tr>
 <tr><td>11:00-11:30</td>
-    <td rowspan="3" bgcolor="#b9f8fa">Session 2.2<br>Talk &amp; Discussion</td>
-    <td rowspan="3" bgcolor="#b9f8fa">Session 3.2<br>Talk &amp; Discussion</td>
-    <td rowspan="3" bgcolor="#b9f8fa">Session 4.2<br>Talk &amp; Discussion</td>
-    <td rowspan="2" bgcolor="#d9a1ff">Wrap up Discussion</td></tr>
+    <td rowspan="3" bgcolor="#b9f8fa">Session 2.2<br>Talks &amp; Discussion</td>
+    <td rowspan="3" bgcolor="#b9f8fa">Session 3.2<br>Talks &amp; Discussion</td>
+    <td rowspan="3" bgcolor="#b9f8fa">Session 4.2<br>Talks &amp; Discussion</td>
+    <td rowspan="2" bgcolor="#d9a1ff">Wrap-up Discussion</td></tr>
 <tr><td>11:30-12:00</td></tr>
 <tr><td>12:00-12:30</td>
     <td rowspan="1" bgcolor="#faf7a5">Closing</td></tr>
