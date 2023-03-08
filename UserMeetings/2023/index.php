0d530a386587b1dcf423be13b212845bd48240b9
Philippe Bourdin <11717935+PABourdin@users.noreply.github.com>
2023-03-08 17:21:09 +0100

    Changed some colors.

--- UserMeetings/2023/index.php
+++ UserMeetings/2023/index.php
@@ -19,7 +19,7 @@ Please find here the <a href="/UserMeetings/2023/agenda.txt">agenda.txt</a> and 
 <table border="1" cellpadding="4" cellspacing="1" style="width:100%">
 
 <tr><td></td><td>Monday</td><td>Tuesday</td><td>Wednesday</td><td>Thursday</td><td>Friday</td></tr>
-</tr><td>09:00-09:30</td><td rowspan="9" bgcolor="#faf7a5">Meet & Greet</td><td rowspan="3" bgcolor="#b9f8fa">Session 2.1 Talk & Discussion</td><td rowspan="3" bgcolor="#b9f8fa">Session 3.1 Talk & Discussion</td><td rowspan="3" bgcolor="#b9f8fa">Session 4.1 Talk & Discussion</td><td rowspan="3" bgcolor="#b9f8fa">Session 5.1 Talk & Discussion</td></tr>
+</tr><td>09:00-09:30</td><td rowspan="9" bgcolor="#ddffa1">Meet & Greet</td><td rowspan="3" bgcolor="#b9f8fa">Session 2.1 Talk & Discussion</td><td rowspan="3" bgcolor="#b9f8fa">Session 3.1 Talk & Discussion</td><td rowspan="3" bgcolor="#b9f8fa">Session 4.1 Talk & Discussion</td><td rowspan="3" bgcolor="#b9f8fa">Session 5.1 Talk & Discussion</td></tr>
 </tr><td>09:30-10:00</td></tr>
 </tr><td>10:00-10:30</td></tr>
 <tr><td>10:30-11:00</td><td rowspan="1" bgcolor="#f0a080">Coffee Break</td></td rowspan="1" bgcolor="#f0a080">Coffee Break</td><td rowspan="1" bgcolor="#f0a080">Coffee Break</td><td rowspan="1" bgcolor="#f0a080">Coffee Break</td><td rowspan="1" bgcolor="#f0a080">Coffee Break</td></tr>
@@ -28,7 +28,7 @@ Please find here the <a href="/UserMeetings/2023/agenda.txt">agenda.txt</a> and 
 <tr><td>12:00-12:30</td><td rowspan="1" bgcolor="#faf7a5">Closing</td></tr>
 <tr><td>12:30-13:00</td><td rowspan="2" bgcolor="#f0a080">Lunch Break</td></td rowspan="2" bgcolor="#f0a080">Lunch Break</td><td rowspan="2" bgcolor="#f0a080">Lunch Break</td><td rowspan="2" bgcolor="#f0a080">Lunch Break</td><td rowspan="2" bgcolor="#f0a080">Lunch Break</td></tr>
 <tr><td>13:00-13:30</td></tr>
-<tr><td>13:30-14:00</td><td rowspan="1" bgcolor="#ddffa1">Open Remarks</td><td rowspan="4" bgcolor="#b9f8fa">Session 2.3 Coding & Discussion</td><td rowspan="11" bgcolor="#b7faa5">Excursion</td><td rowspan="4" bgcolor="#b9f8fa">Session 4.3 Coding & Discussion</td><td rowspan="9" bgcolor="#b9f8fa">Individual Coding Session</td></tr>
+<tr><td>13:30-14:00</td><td rowspan="1" bgcolor="#faf7a5">Open Remarks</td><td rowspan="4" bgcolor="#b9f8fa">Session 2.3 Coding & Discussion</td><td rowspan="11" bgcolor="#b7faa5">Excursion</td><td rowspan="4" bgcolor="#b9f8fa">Session 4.3 Coding & Discussion</td><td rowspan="9" bgcolor="#b9f8fa">Individual Coding Session</td></tr>
 <tr><td>14:00-14:30</td><td rowspan="1" bgcolor="#a1a1ff">Status Report</td></tr>
 <tr><td>14:30-15:00</td><td rowspan="2" bgcolor="#b9f8fa">technical session 1.1 Coding & Discussion</td></tr>
 <tr><td>15:00-15:30</td></tr>
