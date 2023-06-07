07390a18c0464c2d5eb285156641e09cfd3adbc8
Philippe Bourdin <11717935+PABourdin@users.noreply.github.com>
2023-06-07 14:03:51 +0200

    Updated participation (online/in person).

--- UserMeetings/2023/index.php
+++ UserMeetings/2023/index.php
@@ -96,7 +96,7 @@ Please find here the <a href="/UserMeetings/2023/agenda.txt">agenda.txt</a> and 
 <li>LOC: Philippe-A. Bourdin, IGAM/Graz</li>
 <li>-----</li>
 <li>Axel Brandenburg, NORDITA/Stockholm</li>
-<li>Piyali Chatterjee, Indian Institute of Astrophysics</li>
+<li>Piyali Chatterjee (online), Indian Institute of Astrophysics</li>
 <li>Yutong He, NORDITA/Stockholm</li>
 <li>Antonino Salvino Midiri, Université de Genève</li>
 <li>Dhrubaditya Mitra (online), NORDITA/Stockholm</li>
