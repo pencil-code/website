ec495269805f6ba8b88f4bfeafd422db21e49207
Philippe Bourdin <11717935+PABourdin@users.noreply.github.com>
2023-09-06 23:01:28 +0200

    Updated schedule.

--- UserMeetings/2023/index.php
+++ UserMeetings/2023/index.php
@@ -70,20 +70,20 @@ A taxi to the airport costs about 30&nbsp;&euro; and takes about 20&nbsp;mins.
 <tr bgcolor="#f0a080"><td valign="top">10:30</td><td valign="top">Coffee break</td></tr>
 <tr><td valign="top">11:00</td><td valign="top">Ask-Me-Anything, general Q&amp;A Session</td></tr>
 <tr bgcolor="#f0a080"><td valign="top">12:15</td><td valign="top">Grab your Lunch</td></tr>
-<tr><td valign="top">12:30</td><td valign="top">Excursion (Köflach &amp; Piber) &amp; Dinner at Heurigenschänke &quot;Sattler&quot; (Graz)<br>We leave 12:40 with Bus 63 from "Universität" or 13:06 from track 9 at Graz main station.<br>We take tram 7 from Graz main station at 19:08 for "LKH" and at "Klinikum Mitte" we change for Bus 82 to arrive at "Schmied König" at 19:38. Arrival at "Heurigenschänke Sattler" about 19:45.</td></tr>
+<tr><td valign="top">12:30</td><td valign="top">Excursion (Köflach - Piber) &amp; Dinner at Heurigenschänke &quot;Sattler&quot;</td></tr>
 
 <tr><td rowspan="8" bgcolor="#c0c0c0">Thursday, 7</td>
     <td valign="top">09:00</td><td valign="top">Hands-on tutorial for beginners:<br>Running an example and data reading in IDL, Python, Mathematica<br>Break-out rooms: Coding Session</td></tr>
 <tr bgcolor="#f0a080"><td valign="top">10:30</td><td valign="top">Coffee break</td></tr>
-<tr><td valign="top">11:00</td><td valign="top">Coding Session &amp; Tutorials for beginners</td></tr>
+<tr><td valign="top">11:00</td><td valign="top">Work on Agenda items</td></tr>
 <tr bgcolor="#f0a080"><td valign="top">12:15</td><td valign="top">Lunch break</td></tr>
-<tr><td valign="top">13:30</td><td valign="top">Discussion &amp; Coding Session</td></tr>
+<tr><td valign="top">13:30</td><td valign="top">Coding Session</td></tr>
 <tr bgcolor="#f0a080"><td valign="top">15:30</td><td valign="top">Coffee break</td></tr>
-<tr><td valign="top">16:00</td><td valign="top">Discussion &amp; Coding Session</td></tr>
+<tr><td valign="top">16:00</td><td valign="top">Coding Session</td></tr>
 <tr bgcolor="#f0a080"><td valign="top">18:00</td><td valign="top">Barbeque at the Lustbühel Observatory</td></tr>
 
 <tr><td rowspan="9" bgcolor="#f0f0f0">Friday, 8</td>
-    <td valign="top">09:00</td><td valign="top">Discussion &amp; Coding Session</td></tr>
+    <td valign="top">09:00</td><td valign="top">Coding Session</td></tr>
 <tr bgcolor="#f0a080"><td valign="top">10:30</td><td valign="top">Coffee break</td></tr>
 <tr><td valign="top">11:00</td><td valign="top">Session reports</td></tr>
 <tr><td valign="top">11:30</td><td valign="top">Agenda wrap-up</td></tr>
