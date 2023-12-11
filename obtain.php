69834814c32cf11e84ae21c441f49364ba1a5f6a
Philippe Bourdin <11717935+PABourdin@users.noreply.github.com>
2023-12-11 11:47:07 +0100

    Corrected display of SVN checkout line.

--- obtain.php
+++ obtain.php
@@ -4,16 +4,16 @@
 
 	$revision = filter_var (ltrim ($_POST["revision"], 'r'), FILTER_SANITIZE_NUMBER_INT);
 	$line = system ("grep -P \"^r32000\\t\" rev-table_GH-SVN.csv");
-	$new = substr ($line, strpos ($line, "\t"));
+	$new = ltrim (substr ($line, strpos ($line, "\t") + 1), 'r');
  ?>
 <div class="centcolumnpad">
 <h2>Obtain an old revision</h2>
 
 <h3>&bull; SVN checkout:</h3>
 SVN read-only access:
-<div class="codescroll"><code>svn checkout -r<?php echo $new; ?> https://pencil-code.org/svn/playground/trunk playground</code></div>
+<div class="codescroll"><code>svn checkout -r <?php echo $new; ?> https://pencil-code.org/svn/playground/trunk playground</code></div>
 SVN read &amp; write access:
-<div class="codescroll"><code>svn checkout -r<?php echo $new; ?> --username=GITHUB_LOGIN https://pencil-code.org/svn/playground/trunk playground</code></div>
+<div class="codescroll"><code>svn checkout -r <?php echo $new; ?> --username=GITHUB_LOGIN https://pencil-code.org/svn/playground/trunk playground</code></div>
 
 <div class="centerdivider"></div>
 <div class="centcolumnpad">
