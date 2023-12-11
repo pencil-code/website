b91ef1f70d00d2d0962855b5b7a52180dfc7800c
Philippe Bourdin <11717935+PABourdin@users.noreply.github.com>
2023-12-11 23:35:11 +0100

    Updated and simplified.

--- download.php
+++ download.php
@@ -11,14 +11,14 @@ The read-only version you can check out by:
 <div class="codescroll"><code>svn checkout http://github.com/pencil-code/pencil-code/trunk pencil-code</code></div>
 
 For a read-write access, you need to have a github user name ('GITHUB_USER') and become part of the <em>pencil-code</em> group.
-<div class="codescroll"><code>svn checkout https://github.com/pencil-code/pencil-code/trunk pencil-code --username 'GITHUB_USER'</code></div>
+<div class="codescroll"><code>svn checkout https://github.com/pencil-code/pencil-code/trunk pencil-code --username='GITHUB_USER'</code></div>
 
 <h3>&bull; GIT checkout:</h3>
 If you just want to check out the latest version of the code use:
 <div class="codescroll"><code>git clone http://github.com/pencil-code/pencil-code.git</code></div>
 
-If you have a github username 'GITHUB_USER' and like to submit changes you can use:
-<div class="codescroll"><code>git clone https://GITHUB_USER@github.com/pencil-code/pencil-code.git<br>
+If you have a github username 'GITHUB_LOGIN' and like to submit changes you can use:
+<div class="codescroll"><code>git clone https://GITHUB_LOGIN@github.com/pencil-code/pencil-code.git<br>
 git config --global credential.helper 'cache --timeout=3600'<br>
 git config --global branch.autosetuprebase always</code></div>
 With the second line, you set the password cache to one hour, the third line should actually become a default.
@@ -30,7 +30,7 @@ Or, if you prefer to use a SSH keyfile so that you do not have to type your pass
 
 <h3>&bull; Playground:</h3>
 SVN access:
-<div class="codescroll"><code>svn checkout https://pencil-code.org/svn/playground/trunk playground --username=GITHUB_LOGIN</code></div>
+<div class="codescroll"><code>svn checkout https://pencil-code.org/svn/playground/trunk playground --username='GITHUB_LOGIN'</code></div>
 
 GIT access:
 <div class="codescroll"><code>git clone https://GITHUB_LOGIN@pencil-code.org/git/playground</code></div>
