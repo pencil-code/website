36825c63ab7507f66349c1529e0b54a32cb62c93
Philippe Bourdin <11717935+PABourdin@users.noreply.github.com>
2023-12-11 11:50:06 +0100

    Searching now for correct revision number.

--- obtain.php
+++ obtain.php
@@ -3,7 +3,7 @@
 	include "inc/header.inc";
 
 	$revision = filter_var (ltrim ($_POST["revision"], 'r'), FILTER_SANITIZE_NUMBER_INT);
-	$line = system ("grep -P \"^r32000\\t\" rev-table_GH-SVN.csv");
+	$line = system ("grep -P \"^r$revision\\t\" rev-table_GH-SVN.csv 2>&1");
 	$new = ltrim (substr ($line, strpos ($line, "\t") + 1), 'r');
  ?>
 <div class="centcolumnpad">
