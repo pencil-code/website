24121757b0feb9087a2a18c778305c561f208c06
Philippe Bourdin <11717935+PABourdin@users.noreply.github.com>
2023-12-30 21:39:41 +0100

    Design update.

--- transition.php
+++ transition.php
@@ -11,11 +11,11 @@ To submit your changes after the 2nd of January, 13:00 CET, please:
 If your have already committed changes to the Pencil Code, pick up your prepared account at <a href="https://account.pencil-code.org/">account.pencil-code.org</a> with your known GitHub username and the email address to which we have sent the notification email with the verification hash. If you are a new users, please <a href="https://account.pencil-code.org/register-user.html">register a new account</a>, ideally with the same username as on GitHub.
 
 <h3>Step 2)</h3>
-<h4>for <em>GIT users</em></h4>
+<h4>=&gt; for <em>GIT</em> users</h4>
 After <b>2nd of January, 13:00 CET</b> please change the repository URL:
 <div class="codescroll"><code>git remote set-url origin https://USERNAME@pencil-code.org/git/</code></div>
 
-<h4>for <em>SVN users</em></h4>
+<h4>=&gt; for <em>SVN</em> users</h4>
 Either submit all you changes before <b>2nd of January, 13:00 CET</b> to GitHub, or transfer the changes between two working copies: one up-to-date checkout from GitHub and one from pencil-code.org.
 Let us say you have your old GitHub working copy in &quot;pencil-code-old&quot;. Now checkout a fresh working copy from pencil-code.org in &quot;pencil-code-new&quot;:
 <div class="codescroll"><code>svn checkout --username=USERNAME https://pencil-code.org/svn/trunk pencil-code-new</code></div>
