a713be48152c7e7f8d693b460c86c06fa6bfc9b4
Philippe Bourdin <11717935+PABourdin@users.noreply.github.com>
2023-12-30 21:38:01 +0100

    Design update.

--- download.php
+++ download.php
@@ -4,7 +4,7 @@
  ?>
 <div class="centcolumnpad">
 <h2>Transition to pencil-code.org</h2>
-<b>Important:</b> from the <em>2nd to the 7th of January 2024</em> we perform the transfer of our repository to the new domain &quot;pencil-code.org&quot;.
+<b>Important:</b> from the <b>2nd to the 7th of January 2024</b> we perform the transfer of our repository to the new domain &quot;pencil-code.org&quot;.
 Please follow these <a href="/transition.php">instructions to submit your changes after the 2nd of January</a>, 13:00 CET.
 
 <h2>Download</h2>
