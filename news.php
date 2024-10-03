b274224adcc9ca6a550e0f8ecc1068cae7e282d3
Axel Brandenburg <brandenb@nordita.org>
2024-10-03 16:12:21 +0000

    current version; message generated automatically on: Linux nl6 (brandenb)

--- news.php
+++ news.php
@@ -8,7 +8,7 @@
 
 <li class="separated">9 May 2024:<br>
 The current Pencil Code Newsletter is on:
-<a href="https://www.nordita.org/~brandenb/pencil-code/newsletter/">https://www.nordita.org/~brandenb/pencil-code/newsletter/</a>.
+<a href="http://pencil-code.nordita.org/~brandenb/pencil-code/newsletter/">http://pencil-code.nordita.org/~brandenb/pencil-code/newsletter/</a>.
 </li>
 
 <li class="separated">9 May 2024:<br>
@@ -19,7 +19,7 @@ The next Pencil Code User Meeting will be at the
 <li class="separated">23 May 2024:<br>
 The next Pencil Code Steering Committee (PCSC) meeting will be on 6th of June 2024, starting from 13:00 CEST.
 The minutes of the previous meeting are posted on:
-<a href="http://www.nordita.org/~brandenb/pencil-code/PCSC/">http://www.nordita.org/~brandenb/pencil-code/PCSC/</a>
+<a href="http://pencil-code.nordita.org/~brandenb/pencil-code/PCSC/">http://pencil-code.nordita.org/~brandenb/pencil-code/newsletter/PCSC/</a>
 </li>
 
 <li class="separated">Link to <a href="/previous_news_items.php">previous news items</a>.</li>
