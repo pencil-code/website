d2acd750585fc6f93a0477befd0a473f6eb3edfc
Philippe-A. Bourdin <git@Bourdin.ch>
2025-04-02 13:41:24 +0000

    Putting order with most recent server first, most outdated last.

--- tests.php
+++ tests.php
@@ -22,13 +22,13 @@ our own tests. The latest test results for a can be seen online:</p>
 <li><a href="https://norlx51.nordita.org/tests/normal/">Nordita: GNU gfortran 13.3.0 (norlx51, by Philippe/Axel)</a>, normal, **:15 (<a href="https://norlx51.nordita.org/tests/normal-previous/">previous run</a>)
 <!-- Daily test -->
 <li><a href="https://norlx51.nordita.org/tests/extended/">Nordita: GNU gfortran 13.3.0 (norlx51, by Philippe/Axel)</a>, extended, **:55 (<a href="https://norlx51.nordita.org/tests/extended-previous/">previous run</a>)
+<!-- Philippe Bourdin -->
+<li><a href="http://www.PAB-Software.de/Pencil/minimal/">GNU gfortran 9.4.0, (Ubuntu, by Philippe Bourdin)</a>, minimal, minutely (<a href="http://www.PAB-Software.de/Pencil/minimal-previous/">previous run</A>)
+<li><a href="http://www.PAB-Software.de/Pencil/daily/">GNU gfortran 9.4.0, (Ubuntu, by Philippe Bourdin)</a>, all tests, every 6 hours (<a href="http://www.PAB-Software.de/Pencil/daily-previous/">previous run</A>)
 <!-- Hourly test -->
 <li><a href="http://norlx29.nordita.org/~brandenb/pencil-code/tests/hourly/">Nordita: GNU gfortran 6.4.0 with OpenMPI (old server, by Wolfgang/Axel)</a>, hourly (<a href="http://norlx29.nordita.org/~brandenb/pencil-code/tests/hourly-previous/">previous run</a>)
 <!-- Daily test -->
 <li><a href="http://norlx29.nordita.org/~brandenb/pencil-code/tests/daily/">Nordita: GNU gfortran 6.4.0 with OpenMPI (old server, by Wolfgang/Axel)</a>, daily (<a href="http://norlx29.nordita.org/~brandenb/pencil-code/tests/daily-previous/">previous run</a>)
-<!-- Philippe Bourdin -->
-<li><a href="http://www.PAB-Software.de/Pencil/minimal/">GNU gfortran 9.4.0, (Ubuntu, by Philippe Bourdin)</a>, minimal, minutely (<a href="http://www.PAB-Software.de/Pencil/minimal-previous/">previous run</A>)
-<li><a href="http://www.PAB-Software.de/Pencil/daily/">GNU gfortran 9.4.0, (Ubuntu, by Philippe Bourdin)</a>, all tests, every 6 hours (<a href="http://www.PAB-Software.de/Pencil/daily-previous/">previous run</A>)
 <!-- Boris Dintrans (March 2021: 404 not found) -->
 <!--li><a href="http://userpages.irap.omp.eu/~bdintrans/tmp/test_runs.html">Toulouse: Intel ifort v12.1.3.293 (Ubuntu 13.10, by Boris Dintrans)</a>, daily standard level 2 test -->
 <!-- Boris Dintrans additional (March 2021: 404 not found) -->
