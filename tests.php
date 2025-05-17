7405c398e3e9559d19d25c104e3ad4bc47d68fb7
Philippe-A. Bourdin <git@Bourdin.ch>
2025-05-17 14:29:55 +0000

    Added timing information to overview table.

--- tests.php
+++ tests.php
@@ -74,36 +74,43 @@ the very minimal auto-tests:</p>
 <table border="0" cellspacing="10" cellpadding="0">
 <tr><td><b>level</b></td>
     <td><b>name</b></td><td><b>description</b></td>
+    <td><b>time</b></td>
     <td><b>runs</b></td>
     <td><b>host</b></td>
 </tr>
 <tr><td>0</td>
     <td>minimal</td><td>no- + most-modules</td>
+    <td>minutely</td>
     <td><a href="https://www.PAB-Software.de/Pencil/minimal/">latest</a> (<a href="https://www.PAB-Software.de/Pencil/minimal-previous/">previous</a>)</td>
     <td>pencil-code.org</td>
 </tr>
 <tr><td>0+1</td>
     <td>basic</td><td>same as Travis test</td>
+    <td>minutely</td>
     <td><a href="https://norlx51.nordita.org/tests/basic/">latest</a> (<a href="https://norlx51.nordita.org/tests/basic-previous/">previous</a>)</td>
     <td>Norlx51</td>
 </tr>
 <tr><td>2</td>
     <td>normal</td><td>without basic</td>
+    <td>**:15</td>
     <td><a href="https://norlx51.nordita.org/tests/normal/">latest</a> (<a href="https://norlx51.nordita.org/tests/normal-previous/">previous</a>)</td>
     <td>Norlx51</td>
 </tr>
 <tr><td>0-2</td>
     <td>default</td><td>basic + normal</td>
+    <td>*/2:03</td>
     <td><a href="http://norlx29.nordita.org/~brandenb/pencil-code/tests/hourly/">latest</a> (<a href="http://norlx29.nordita.org/~brandenb/pencil-code/tests/hourly-previous/">previous</a>)</td>
     <td>Norlx29</td>
 </tr>
 <tr><td>3</td>
     <td>extended</td><td>without default</td>
+    <td>**:55</td>
     <td><a href="https://norlx51.nordita.org/tests/extended/">latest</a> (<a href="https://norlx51.nordita.org/tests/extended-previous/">previous</a>)</td>
     <td>Norlx51</td>
 </tr>
 <tr><td>0-3</td>
     <td>all tests</td><td>default + extended</td>
+    <td>*/6:31</td>
     <td><a href="https://www.PAB-Software.de/Pencil/daily/">latest</a> (<a href="https://www.PAB-Software.de/Pencil/daily-previous/">previous</a>)</td>
     <td>pencil-code.org</td>
 </tr>
@@ -111,9 +118,11 @@ the very minimal auto-tests:</p>
     <td></td><td>overlong runtime</td>
     <td></td>
     <td></td>
+    <td></td>
 </tr>
 <tr><td>5-...</td>
     <td></td><td>deactivated</td>
+    <td></td>
     <td></td>
     <td></td>
 </tr>
