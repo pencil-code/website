0371cf9f0b9a97d0f56f71e3ec463797fbb5c061
Philippe-A. Bourdin <git@Bourdin.ch>
2025-05-23 21:08:22 +0000

    Changed working in overview to be more descriptive.

--- tests.php
+++ tests.php
@@ -80,7 +80,7 @@ our own tests. The latest test results for a can be seen online:</p>
     <td>Axel/Philippe</td>
 </tr>
 <tr><td>5</td>
-    <td>overlong</td><td>run and succeede</td>
+    <td>overlong</td><td>should succeede</td>
     <td>15:31</td>
     <td><a href="http://norlx29.nordita.org/~pencil/overlong/">latest</a> (<a href="http://norlx29.nordita.org/~pencil/overlong-previous/">previous</a>)</td>
     <td>Norlx29</td>
@@ -88,7 +88,7 @@ our own tests. The latest test results for a can be seen online:</p>
     <td>Philippe/Axel</td>
 </tr>
 <tr><td>6-9</td>
-    <td>defective</td><td>deactivated + broken</td>
+    <td>defective</td><td>known to fail</td>
     <td>03:31</td>
     <td><a href="http://norlx29.nordita.org/~pencil/defective/">latest</a> (<a href="http://norlx29.nordita.org/~pencil/defective-previous/">previous</a>)</td>
     <td>Norlx29</td>
