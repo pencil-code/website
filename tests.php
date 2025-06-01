cf27676a0d05d77aacd4f57461fdf3477ed15ac2
Philippe-A. Bourdin <git@Bourdin.ch>
2025-06-01 21:49:45 +0000

    Running "fixme" test more often.

--- tests.php
+++ tests.php
@@ -80,7 +80,7 @@ our own tests. The latest test results for a can be seen online:</p>
 </tr>
 <tr><td>4</td>
     <td>fixme</td><td>succeeded before</td>
-    <td>*/9:45</td>
+    <td>*/6:45</td>
     <td><img src="https://norlx51.nordita.org/tests/fixme/result.svg" WIDTH="16" HEIGHT="16" ALT="?"></td>
     <td><a href="https://norlx51.nordita.org/tests/fixme/">latest</a> (<a href="https://norlx51.nordita.org/tests/fixme-previous/">previous</a>)</td>
     <td>Norlx51</td>
