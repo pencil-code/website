bbf49247f389940e9d33ed26869ea878b13eb8f4
Philippe-A. Bourdin <git@Bourdin.ch>
2025-07-21 20:18:34 +0000

    Re-inserting the HTML tag for missing images.

--- tests.php
+++ tests.php
@@ -27,7 +27,7 @@ our own tests. The latest test results for a can be seen online:</p>
 <tr><td>0</td>
     <td>minimal</td><td>no- &amp; most-modules</td>
     <td>minutely</td>
-    <td><img src="https://pencil-code.org/tests/minimal/result.svg" WIDTH="16" HEIGHT="16" ALT="oslash;"></td>
+    <td><img src="https://pencil-code.org/tests/minimal/result.svg" WIDTH="16" HEIGHT="16" ALT="&oslash;"></td>
     <td><a href="https://pencil-code.org/tests/minimal/">latest</a> (<a href="https://pencil-code.org/tests/minimal-previous/">previous</a>)</td>
     <td>pencil-code.org</td>
     <td>GNU 9.4</td>
@@ -36,7 +36,7 @@ our own tests. The latest test results for a can be seen online:</p>
 <tr><td>0+1</td>
     <td>basic</td><td>same as <a href="https://github.com/pencil-code/pencil-code/commits/master/">Travis</a></td>
     <td>minutely</td>
-    <td><img src="https://norlx51.nordita.org/tests/basic/result.svg" WIDTH="16" HEIGHT="16" ALT="oslash;"></td>
+    <td><img src="https://norlx51.nordita.org/tests/basic/result.svg" WIDTH="16" HEIGHT="16" ALT="&oslash;"></td>
     <td><a href="https://norlx51.nordita.org/tests/basic/">latest</a> (<a href="https://norlx51.nordita.org/tests/basic-previous/">previous</a>)</td>
     <td>Norlx51</td>
     <td>GNU 13.3</td>
@@ -45,7 +45,7 @@ our own tests. The latest test results for a can be seen online:</p>
 <tr><td>2</td>
     <td>normal</td><td>without basic</td>
     <td>*/*:15</td>
-    <td><img src="https://norlx51.nordita.org/tests/normal/result.svg" WIDTH="16" HEIGHT="16" ALT="oslash;"></td>
+    <td><img src="https://norlx51.nordita.org/tests/normal/result.svg" WIDTH="16" HEIGHT="16" ALT="&oslash;"></td>
     <td><a href="https://norlx51.nordita.org/tests/normal/">latest</a> (<a href="https://norlx51.nordita.org/tests/normal-previous/">previous</a>)</td>
     <td>Norlx51</td>
     <td>GNU 13.3</td>
@@ -54,7 +54,7 @@ our own tests. The latest test results for a can be seen online:</p>
 <tr><td>0-2</td>
     <td>default</td><td>basic + normal</td>
     <td>*/2:03</td>
-    <td><img src="https://norlx65.nordita.org/tests/default/result.svg" WIDTH="16" HEIGHT="16" ALT="oslash;"></td>
+    <td><img src="https://norlx65.nordita.org/tests/default/result.svg" WIDTH="16" HEIGHT="16" ALT="&oslash;"></td>
     <td><a href="https://norlx65.nordita.org/tests/default/">latest</a> (<a href="https://norlx65.nordita.org/tests/default-previous/">previous</a>)</td>
     <td>Norlx65</td>
     <td>GNU 13.3</td>
@@ -63,7 +63,7 @@ our own tests. The latest test results for a can be seen online:</p>
 <tr><td>3</td>
     <td>extended</td><td>without default</td>
     <td>*/*:55</td>
-    <td><img src="https://norlx51.nordita.org/tests/extended/result.svg" WIDTH="16" HEIGHT="16" ALT="oslash;"></td>
+    <td><img src="https://norlx51.nordita.org/tests/extended/result.svg" WIDTH="16" HEIGHT="16" ALT="&oslash;"></td>
     <td><a href="https://norlx51.nordita.org/tests/extended/">latest</a> (<a href="https://norlx51.nordita.org/tests/extended-previous/">previous</a>)</td>
     <td>Norlx51</td>
     <td>GNU 13.3</td>
@@ -72,7 +72,7 @@ our own tests. The latest test results for a can be seen online:</p>
 <tr><td>0-3</td>
     <td>full test</td><td>default + extended</td>
     <td>*/6:31</td>
-    <td><img src="https://pencil-code.org/tests/full/result.svg" WIDTH="16" HEIGHT="16" ALT="oslash;"></td>
+    <td><img src="https://pencil-code.org/tests/full/result.svg" WIDTH="16" HEIGHT="16" ALT="&oslash;"></td>
     <td><a href="https://pencil-code.org/tests/full/">latest</a> (<a href="https://pencil-code.org/tests/full-previous/">previous</a>)</td>
     <td>pencil-code.org</td>
     <td>GNU 9.4</td>
@@ -81,7 +81,7 @@ our own tests. The latest test results for a can be seen online:</p>
 <tr><td>4</td>
     <td>fixme</td><td>succeeded before</td>
     <td>*/6:45</td>
-    <td><img src="https://norlx51.nordita.org/tests/fixme/result.svg" WIDTH="16" HEIGHT="16" ALT="oslash;"></td>
+    <td><img src="https://norlx51.nordita.org/tests/fixme/result.svg" WIDTH="16" HEIGHT="16" ALT="&oslash;"></td>
     <td><a href="https://norlx51.nordita.org/tests/fixme/">latest</a> (<a href="https://norlx51.nordita.org/tests/fixme-previous/">previous</a>)</td>
     <td>Norlx51</td>
     <td>GNU 13.3</td>
@@ -90,7 +90,7 @@ our own tests. The latest test results for a can be seen online:</p>
 <tr><td>5</td>
     <td>overlong</td><td>runs less often</td>
     <td>15:31</td>
-    <td><img src="https://norlx65.nordita.org/tests/overlong/result.svg" WIDTH="16" HEIGHT="16" ALT="oslash;"></td>
+    <td><img src="https://norlx65.nordita.org/tests/overlong/result.svg" WIDTH="16" HEIGHT="16" ALT="&oslash;"></td>
     <td><a href="https://norlx65.nordita.org/tests/overlong/">latest</a> (<a href="https://norlx65.nordita.org/tests/overlong-previous/">previous</a>)</td>
     <td>Norlx65</td>
     <td>GNU 13.3</td>
@@ -99,7 +99,7 @@ our own tests. The latest test results for a can be seen online:</p>
 <tr><td>6-9</td>
     <td>defective</td><td>known to fail</td>
     <td>03:31</td>
-    <td><img src="https://norlx65.nordita.org/tests/defective/result.svg" WIDTH="16" HEIGHT="16" ALT="oslash;"></td>
+    <td><img src="https://norlx65.nordita.org/tests/defective/result.svg" WIDTH="16" HEIGHT="16" ALT="&oslash;"></td>
     <td><a href="https://norlx65.nordita.org/tests/defective/">latest</a> (<a href="https://norlx65.nordita.org/tests/defective-previous/">previous</a>)</td>
     <td>Norlx65</td>
     <td>GNU 13.3</td>
