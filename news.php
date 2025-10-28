b1aee24b46040f5c68648f78c96300ba3acdba04
Philippe-A. Bourdin <git@Bourdin.ch>
2025-10-28 22:59:30 +0000

    Removed outdated date - newsletter should always point to the latest issue.

--- news.php
+++ news.php
@@ -6,7 +6,7 @@
 <h2>News</h2>
 <ul>
 
-<li class="separated">23 March 2025:<br>
+<li class="separated"><!-- 23 March 2025:<br> -->
 The current Pencil Code Newsletter is on:
 <a href="http://pencil-code.nordita.org/~brandenb/pencil-code/newsletter/">http://pencil-code.nordita.org/~brandenb/pencil-code/newsletter/</a>.
 </li>
