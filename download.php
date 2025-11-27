5b919a863ac293bfdeb4790443e0ed143370500b
Philippe-A. Bourdin <git@Bourdin.ch>
2025-11-27 15:02:01 +0000

    Added GIT configuration for name and email address.

--- download.php
+++ download.php
@@ -26,6 +26,10 @@ If you want to submit your changes, please register at <a href="https://account.
 <h4>=&gt; GIT:</h4>
 <div class="codescroll"><code>git clone https://USERNAME@pencil-code.org/git/ pencil-code</code></div>
 
+<p>Be sure to use exactly the same email address and username as in your GitHub account:
+<div class="codescroll"><code>git config --global user.email "my-primary@email.net"
+git config --global user.name "USERNAME"</code></div></p>
+
 <p>To cache your password for #### seconds, you can configure:
 <div class="codescroll"><code>git config --global credential.helper 'cache --timeout=####'</code></div></p>
 
