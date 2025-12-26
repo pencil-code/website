95c49e412e553f87524872d9e68750042232dd66
Axel Brandenburg <brandenb@nordita.org>
2025-12-26 11:16:45 +0000

    current version; message generated automatically on: Linux nl6 (brandenb)

--- samples/1d-tests/sine-Gordon/index.php
+++ samples/1d-tests/sine-Gordon/index.php
@@ -7,12 +7,18 @@
 <h3><tt>samples/sine-Gordon</tt></h3>
 In src/Makefile.local, we set "SPECIAL = special/klein_gordon" to invoke the Klein-Gordon module.
 In run.in, we put Vprime_choice='cos-profile', phimass=2., lambda_phi=1.
-Looking into the subroutine "calc_pencils_special", we see that "p%Vprime=phimass2*lambda_phi*sin(lambda_phi*p%phi)".
+Looking into the subroutine "calc_pencils_special", we see that
+<ul>
+  p%Vprime=phimass2*lambda_phi*sin(lambda_phi*p%phi)
+</ul>
 Here, Vprime refers to the derivative of the potential, so a cosine potential 'cos-profile' means a sine function in the equation.
 Furthermore, phimass2=phimass**2 is a shorthand and "phimass=2." along with "lambda_phi=1." are inputs.
 
 As initial condition, we have "initspecial='phi=atan_exp_kx'".
-In the code, it says that this means "phi=4.*amplphi*atan(exp(phi_gam*kx_phi*(x-offset)))",
+In the code, it says that this means
+<ul>
+  phi=4.*amplphi*atan(exp(phi_gam*kx_phi*(x-offset))),
+</ul>
 where "phi_gam=1./sqrt(1.-phi_v**2)" is an effective wavenumber, as explained in the wikipedia page.
 Here, "phi_v=.9" is an input parameter in start.in, and "kx_phi=2." is another one.
 
