51b5efeb41223a6cf9aba73e86c5659aa534eb19
Clara Dehman <clara.dehman@gmail.com>
2026-03-08 15:48:32 +0000

    Update index.php: improve text, unify paragraph spacing, add poster, and polish registration section

--- UserMeetings/2026/index.php
+++ UserMeetings/2026/index.php
@@ -1,22 +1,26 @@
 <?php
     set_include_path ("../../");
     include "inc/header.inc";
- ?>
+?>
 <div class="centcolumnpad">
 <!-- HEADER -->
 <h2>22<sup>nd</sup> Pencil Code User Meeting 2026</h2>
 
+<p style="font-size:0.9em; color:#555; margin:15px 0 25px 0;">
+The meeting aims to feature technical and scientific presentations, workshops, open discussions on the Pencil Code, and dedicated sessions for collaborative coding and individual consultations.
+</p>
+
 <!-- DATE and VENUE -->
-<p><strong><u>DATE &amp; VENUE:</u></strong><br>
+<p style="margin:15px 0 25px 0;">
+<strong><u>DATE &amp; VENUE:</u></strong><br>
 <strong>21<sup>st</sup> to 25<sup>th</sup> September 2026</strong><br>
 The meeting will be held in <a href="https://es.wikipedia.org/wiki/Alicante">Alicante (Spain)</a> at the <a href="https://www.ua.es">University of Alicante</a>.
 </p>
 
-<!-- OBJECTIVE AND REGISTRATION: -->
-<p><strong><u>OBJECTIVE AND REGISTRATION:</u></strong><br>
-The meeting will feature technical and scientific presentations, workshops, open discussions on the Pencil Code, and dedicated sessions for collaborative coding and individual consultations.
-If you would like to participate, please complete the <a href="https://docs.google.com/forms/d/e/1FAIpQLSdWkVAHUsBLANDjyNDBXh-0rCXKdiTA_GeCclYPCQOVD34Caw/viewform?usp=publish-editor">registration form</a>.
-There is no registration fee.
+<!-- REGISTRATION -->
+<p style="margin:15px 0 25px 0;">
+<strong><u>REGISTRATION:</u></strong><br>
+Participants are invited to register by completing the <a href="https://docs.google.com/forms/d/e/1FAIpQLSdWkVAHUsBLANDjyNDBXh-0rCXKdiTA_GeCclYPCQOVD34Caw/viewform?usp=publish-editor" target="_blank">online registration form</a>. Registration is free of charge.
 </p>
 
 <!-- POSTER -->
@@ -27,12 +31,12 @@ There is no registration fee.
     <p style="font-size: 0.9em; color: #555; margin-top: 8px;">Click to view full-size poster</p>
 </div>
 
-
-<!-- DETAILED SCHEDULE & TALKS  -->
-<p><strong><u>DETAILED SCHEDULE WILL BE PROVIDED AT A LATER TIME</u></strong></p>
-
+<!-- DETAILED SCHEDULE & TALKS -->
+<p style="margin:15px 0 25px 0;">
+<strong><u>DETAILED SCHEDULE WILL BE PROVIDED AT A LATER TIME</u></strong>
+</p>
 
 </div>
 <?php
     include "inc/footer.inc";
- ?>
+?>
