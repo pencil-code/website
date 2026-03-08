0696c7af4365f7ea57eaf3b6f33ca2dc1f38491b
Clara Dehman <clara.dehman@gmail.com>
2026-03-08 17:16:44 +0000

    reordering the text

--- UserMeetings/2026/index.php
+++ UserMeetings/2026/index.php
@@ -33,8 +33,9 @@ From there, you can reach the University campus by <strong>Bus 24</strong> or <s
 <!-- ACCOMMODATION -->
 <p style="margin:15px 0 25px 0;">
 <strong><u>ACCOMMODATION:</u></strong><br>
-We recommend staying either in <strong>Alicante city centre</strong> or near the <strong>University campus</strong> in Sant Vicent del Raspeig. Early booking is advised due to Alicante’s high tourist season.<br>
-A list of recommended accommodations can be found on the <a href="https://web.ua.es/en/estidia22/accommodation.html" target="_blank">University website</a>.
+We recommend staying either in <strong>Alicante city centre</strong> or near the <strong>University campus</strong> in Sant Vicent del Raspeig. <br>
+A list of recommended accommodations can be found on the <a href="https://web.ua.es/en/estidia22/accommodation.html" target="_blank">University website</a>. <br>
+Early booking is advised due to Alicante’s high tourist season.
 </p>
 
 <!-- DETAILED SCHEDULE & TALKS -->
