835c8987c005540d824289173fc48c96e60c72df
Clara Dehman <clara.dehman@gmail.com>
2026-03-08 15:53:25 +0000

    Unify text style and font size.

--- UserMeetings/2026/index.php
+++ UserMeetings/2026/index.php
@@ -6,7 +6,7 @@
 <!-- HEADER -->
 <h2>22<sup>nd</sup> Pencil Code User Meeting 2026</h2>
 
-<p style="font-size:0.9em; color:#555; margin:15px 0 25px 0;">
+<p style="font-style: italic; color:#222; margin:15px 0 25px 0;">
 The meeting aims to feature technical and scientific presentations, workshops, open discussions on the Pencil Code, and dedicated sessions for collaborative coding and individual consultations.
 </p>
 
