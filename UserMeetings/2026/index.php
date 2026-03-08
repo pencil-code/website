0c94b13d2f702c21f811d637ecd91012a4b124ed
Clara Dehman <clara.dehman@gmail.com>
2026-03-08 16:10:59 +0000

    shifting the caption to the rhs and increasing font size a bit. Changing the color of the text below the heading

--- UserMeetings/2026/index.php
+++ UserMeetings/2026/index.php
@@ -6,7 +6,7 @@
 <!-- HEADER -->
 <h2>22<sup>nd</sup> Pencil Code User Meeting 2026</h2>
 
-<p style="font-style: italic; font-weight: bold; color:#708090; margin:0;">
+<p style="font-style: italic; font-weight: bold; color:#607D8B; margin:0;">
 The meeting aims to feature technical and scientific presentations, workshops, open discussions on the Pencil Code, and dedicated sessions for collaborative coding and individual consultations.
 </p>
 
@@ -29,7 +29,7 @@ Registration is free of charge.
     <a href="PCUM26.jpeg" target="_blank">
         <img src="PCUM26.jpeg" alt="PCUM 2026 Poster" style="max-width: 100%; height: auto; border: 1px solid #ccc; box-shadow: 2px 2px 8px rgba(0,0,0,0.2);">
     </a>
-    <p style="font-size: 0.75em; color: #777; margin-top: 4px; font-style: italic;">
+    <p style="font-size: 0.8em; color: #777; margin-top: 4px; font-style: italic; text-align: right;">
         Photo credit: University of Alicante webpage and GetYourGuide
     </p>
 </div>
