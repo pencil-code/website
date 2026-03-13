4a776d6748e1d4b9d51d15b1439bbf0f4b921de0
Axel Brandenburg <brandenb@nordita.org>
2026-03-13 21:01:12 +0000

    +The next Pencil Code User Meeting will be held 21-35 Sep 2026 in Alicante: ...

--- news.php
+++ news.php
@@ -11,6 +11,11 @@ The current Pencil Code Newsletter is on:
 <a href="http://pencil-code.nordita.org/~brandenb/pencil-code/newsletter/">http://pencil-code.nordita.org/~brandenb/pencil-code/newsletter/</a>.
 </li>
 
+<li class="separated"><!-- 13 March 2026:<br> -->
+The next Pencil Code User Meeting will be held 21-35 Sep 2026 in Alicante:
+<a href="https://pencil-code.nordita.org/UserMeetings/2026/">https://pencil-code.nordita.org/UserMeetings/2026/</a>.
+</li>
+
 <li class="separated">Link to <a href="/previous_news_items.php">previous news items</a>.</li>
 
 </ul>
