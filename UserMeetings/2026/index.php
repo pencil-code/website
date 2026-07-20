2b9d5c3ee79d635e57b2d3db65eb044fdfddfa62
Clara Dehman <clara.dehman@gmail.com>
2026-07-20 13:51:56 +0000

    Style improvements

--- UserMeetings/2026/index.php
+++ UserMeetings/2026/index.php
@@ -10,38 +10,49 @@
 The meeting aims to feature technical and scientific presentations, workshops, open discussions on the Pencil Code, and dedicated sessions for collaborative coding and individual consultations.
 </p>
 
+<hr style="border: none; border-top: 1px solid #eee; margin: 25px 0;">
+
 <!-- DATE & VENUE -->
 <p style="margin:15px 0 25px 0;">
-<strong><u>VENUE &amp; DATE:</u></strong><br>
-The PCUM 2026 will take place at the <a href="https://www.ua.es">University of Alicante</a> in <a href="https://es.wikipedia.org/wiki/Alicante">Alicante, Spain</a>, from <strong>21<sup>st</sup> to 25<sup>th</sup> September 2026</strong>.</p>
+<strong style="color:#2c3e50; border-bottom: 2px solid #536066; padding-bottom:2px;">VENUE &amp; DATE:</strong><br><br>
+The PCUM 2026 will take place at the <a href="https://www.ua.es">University of Alicante</a> in <a href="https://es.wikipedia.org/wiki/Alicante">Alicante, Spain</a>, from <strong>21<sup>st</sup> to 25<sup>th</sup> September 2026</strong>.
+</p>
+
+<hr style="border: none; border-top: 1px solid #eee; margin: 25px 0;">
 
 <!-- REGISTRATION -->
 <p style="margin:15px 0 25px 0;">
-<strong><u>REGISTRATION:</u></strong><br>
+<strong style="color:#2c3e50; border-bottom: 2px solid #536066; padding-bottom:2px;">REGISTRATION:</strong><br><br>
 Participants are invited to register by completing the <a href="https://docs.google.com/forms/d/e/1FAIpQLSdWkVAHUsBLANDjyNDBXh-0rCXKdiTA_GeCclYPCQOVD34Caw/viewform?usp=publish-editor" target="_blank">online registration form</a>. <br>
 Registration is free of charge. There is also the possibility of attending the meeting online. A Zoom link will be shared in due time. If you are not familiar with Pencil Code, tutorial sessions will be available, which you can attend either online or in person.
 </p>
+
+<hr style="border: none; border-top: 1px solid #eee; margin: 25px 0;">
 
 <!-- Venue Map and Directions -->
 <div style="display: flex; flex-wrap: wrap; gap: 20px; align-items: stretch; margin: 20px 0;">
 
   <div style="flex: 1; min-width: 300px;">
     <p style="margin:15px 0 25px 0;">
-      <strong><u>HOW TO REACH THE UNIVERSITY OF ALICANTE CAMPUS:</u></strong><br>
-      The recommended airport is <a href="https://www.aena.es/es/alicante-elche-miguel-hernandez.html?utm_source=Google&utm_medium=GMB_ALC" target="_blank">Alicante‑Elche Airport (ALC)</a>. 
+      <strong style="color:#2c3e50; border-bottom: 2px solid #536066; padding-bottom:2px;">HOW TO REACH THE UNIVERSITY OF ALICANTE CAMPUS:</strong><br><br>
+      The recommended airport is <a href="https://www.aena.es/es/alicante-elche-miguel-hernandez.html?utm_source=Google&utm_medium=GMB_ALC" target="_blank">Alicante‑Elche Airport (ALC)</a>.
       From the airport, take the <strong>C‑6 bus</strong> to Alicante city centre.<br>
       From there, you can reach the University campus by either <strong>Bus 24</strong> or <strong>TRAM Line L2</strong>.
     </p>
 
-    <p style="margin:15px 0 25px 0;">
-      <strong>Last bus/tram in the evening:</strong><br>
-      <u>From Alicante Center → Campus:</u><br>
-      - Tram L2: 10:56 pm from Mercat tram stop<br>
-      - Bus 24: 10:39 pm from Mercado‑San Vicente bus stop<br><br>
-      <u>From Campus → Alicante Center:</u><br>
-      - Tram L2: 11:25 pm from University tram stop<br>
-      - Bus 24: 10:47 pm from Calle Perú 4 bus stop
-    </p>
+    <p style="margin:15px 0 5px 0;"><strong>Last bus/tram in the evening:</strong></p>
+
+    <p style="margin:10px 0 5px 0;"><u>From Alicante Center → Campus:</u></p>
+    <ul style="margin:0 0 15px 0;">
+      <li>Tram L2: 10:56 pm from Mercat tram stop</li>
+      <li>Bus 24: 10:39 pm from Mercado‑San Vicente bus stop</li>
+    </ul>
+
+    <p style="margin:10px 0 5px 0;"><u>From Campus → Alicante Center:</u></p>
+    <ul style="margin:0;">
+      <li>Tram L2: 11:25 pm from University tram stop</li>
+      <li>Bus 24: 10:47 pm from Calle Perú 4 bus stop</li>
+    </ul>
   </div>
 
   <div style="flex: 1; min-width: 300px; display: flex; align-items: stretch;">
@@ -52,11 +63,21 @@ Registration is free of charge. There is also the possibility of attending the m
 
 </div>
 
+<hr style="border: none; border-top: 1px solid #eee; margin: 25px 0;">
+
 <!-- ACCOMMODATION -->
-<p style="margin:15px 0 25px 0;">
-  <strong><u>ACCOMMODATION:</u></strong><br>
-  We recommend staying either in <strong>Alicante city centre</strong> or near the <strong>University campus</strong> in Sant Vicent del Raspeig. Early booking is advised due to Alicante’s high tourist season. A list of recommended accommodations can be found on the <a href="https://web.ua.es/en/estidia22/accommodation.html" target="_blank">University website</a>. For those near the campus, we suggest <a href="https://www.villauniversitaria.com" target="_blank">Villa Universitaria Hotel</a>. In the city centre, good options include <a href="https://www.eurostarshotels.co.uk/eurostars-lucentum.html?referer_code=bs2gg11ww&gad_source=1&gad_campaignid=108134338&gbraid=0AAAAAD-VyO7aeD5TNZUs2xtitaywqudMh&gclid=CjwKCAjwg_nNBhAGEiwAiYPYA_qo84_195JfJAT3zWF7Hbwxi1B6I9--O-mdDH3IrZafTRBMP9UDphoCrfYQAvD_BwE" target="_blank">Hotel Eurostars Lucentum</a> and <a href="https://www.themarkethostel.com" target="_blank">The Market Hostel</a>, with plenty of additional options available on <a href="https://www.booking.com" target="_blank">Booking.com</a>.
+<p style="margin:15px 0 5px 0;">
+  <strong style="color:#2c3e50; border-bottom: 2px solid #536066; padding-bottom:2px;">ACCOMMODATION:</strong><br><br>
+  We recommend staying either in <strong>Alicante city centre</strong> or near the <strong>University campus</strong> in Sant Vicent del Raspeig. Early booking is advised due to Alicante's high tourist season.
 </p>
+<ul style="margin:0 0 25px 0;">
+  <li>A list of recommended accommodations can be found on the <a href="https://web.ua.es/en/estidia22/accommodation.html" target="_blank">University website</a>.</li>
+  <li>For those near the campus, we suggest <a href="https://www.villauniversitaria.com" target="_blank">Villa Universitaria Hotel</a>.</li>
+  <li>In the city centre, good options include <a href="https://www.eurostarshotels.co.uk/eurostars-lucentum.html?referer_code=bs2gg11ww&gad_source=1&gad_campaignid=108134338&gbraid=0AAAAAD-VyO7aeD5TNZUs2xtitaywqudMh&gclid=CjwKCAjwg_nNBhAGEiwAiYPYA_qo84_195JfJAT3zWF7Hbwxi1B6I9--O-mdDH3IrZafTRBMP9UDphoCrfYQAvD_BwE" target="_blank">Hotel Eurostars Lucentum</a> and <a href="https://www.themarkethostel.com" target="_blank">The Market Hostel</a>.</li>
+  <li>Plenty of additional options are available on <a href="https://www.booking.com" target="_blank">Booking.com</a>.</li>
+</ul>
+
+<hr style="border: none; border-top: 1px solid #eee; margin: 25px 0;">
 
 <!-- DETAILED SCHEDULE & TALKS -->
 <!-- <p style="margin:15px 0 25px 0;">
@@ -65,19 +86,27 @@ Registration is free of charge. There is also the possibility of attending the m
 
 <!-- CONTACT -->
 <p style="margin:15px 0 25px 0;">
-<strong><u>CONTACT:</u></strong><br>
+<strong style="color:#2c3e50; border-bottom: 2px solid #536066; padding-bottom:2px;">CONTACT:</strong><br><br>
 For any inquiries regarding the meeting, please contact <strong>Clara Dehman</strong> (local organizer) at <a href="mailto:clara.dehman@ua.es">clara.dehman@ua.es</a>.
 </p>
 
+<hr style="border: none; border-top: 1px solid #eee; margin: 25px 0;">
 
 <!-- PARTICIPANTS -->
-<p><b><u>PARTICIPANTS:</u></b></p>
-<ul>
+<p style="margin:15px 0 10px 0;">
+<strong style="color:#2c3e50; border-bottom: 2px solid #536066; padding-bottom:2px;">PARTICIPANTS:</strong>
+</p>
+
+<p style="margin:10px 0 5px 0;"><strong>Organizing Committee:</strong></p>
+<ul style="margin:0 0 20px 0;">
 <li>SOC: Axel Brandenburg, NORDITA, Sweden</li>
 <li>SOC: Philippe-A. Bourdin, University of Graz, Austria</li>
 <li>LOC&amp;SOC: Clara Dehman, University of Alicante, Spain</li>
 <li>SOC: Jennifer Schober, University of Bonn, Germany</li>
-<li>-----</li>
+</ul>
+
+<p style="margin:10px 0 5px 0;"><strong>Participants:</strong></p>
+<ul style="margin:0;">
 <li>Kent Barbey, University of Bonn, Germany</li>
 <li>Deepen Garg, University of Bonn, Germany</li>
 <li>Kishore Gopalakrishnan (online), IIT Madras, India</li>
@@ -88,13 +117,14 @@ For any inquiries regarding the meeting, please contact <strong>Clara Dehman</st
 <li>Vasco Silver, University of Bonn, Germany</li>
 </ul>
 
+<hr style="border: none; border-top: 1px solid #eee; margin: 25px 0;">
 
 <!-- POSTER -->
 <div style="text-align: center; margin: 30px 0;">
     <a href="PCUM26.jpeg" target="_blank">
         <img src="PCUM26.jpeg" alt="PCUM 2026 Poster" style="max-width: 100%; height: auto; border: 1px solid #ccc; box-shadow: 2px 2px 8px rgba(0,0,0,0.2);">
     </a>
-    <p style="font-size: 0.8em; color: #777; margin-top: 4px; font-style: italic; text-align: right;">
+    <p style="font-size: 0.8em; color: #777; margin-top: 4px; font-style: italic; text-align: center;">
         Photo credit: University of Alicante webpage and GetYourGuide
     </p>
 </div>
